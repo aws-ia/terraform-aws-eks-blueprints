4c44394f579c16f6bf20b1e517da3a54a7d9ee66
Vara Bonthu <vara.bonthu@gmail.com>
2021-10-12 21:58:21 +0100

    Cluster Autoscaler resource name corrected

--- kubernetes-addons/cluster-autoscaler/main.tf
+++ kubernetes-addons/cluster-autoscaler/main.tf
@@ -17,7 +17,7 @@
  */
 
 
-resource "helm_release" "metrics_server" {
+resource "helm_release" "cluster_autoscaler" {
   name                       = local.cluster_autoscaler_helm_app["name"]
   repository                 = local.cluster_autoscaler_helm_app["repository"]
   chart                      = local.cluster_autoscaler_helm_app["chart"]
