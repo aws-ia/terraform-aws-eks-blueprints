3b992d7a5726f40dd9b15346a1566649532697e6
Kevin Coleman <kcaws@amazon.com>
2021-11-18 20:38:26 -0800

    Formatting files

--- kubernetes-addons/aws-for-fluentbit/outputs.tf
+++ kubernetes-addons/aws-for-fluentbit/outputs.tf
@@ -25,8 +25,8 @@ output "aws_fluent_bit_cw_log_group_arn" {
   description = "AWS Fluent Bit CloudWatch Log Group ARN"
   value       = aws_cloudwatch_log_group.eks_worker_logs.arn
 }
- 
+
 output "argocd_gitops_config" {
   description = "Configuration used for managing the add-on with GitOps"
-  value = var.manage_via_gitops ? local.argocd_gitops_config : null
+  value       = var.manage_via_gitops ? local.argocd_gitops_config : null
 }
