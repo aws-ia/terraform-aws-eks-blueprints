fdae1cefbf161078e4337001642f3d5e929d9a6c
Vara Bonthu <vara.bonthu@gmail.com>
2021-11-23 22:05:32 +0000

    minimum policies for node groups

--- modules/aws-eks-managed-node-groups/data.tf
+++ modules/aws-eks-managed-node-groups/data.tf
@@ -17,3 +17,37 @@ data "aws_iam_policy_document" "managed_ng_assume_role_policy" {
     }
   }
 }
+//Failed to refresh cloud provider config: cannot k8s.io/cluster-autoscaler/:
+//User: arn:aws:sts::327949925549:assumed-role/aws001-preprod-test-eks-managed-ondemand/i-05e6f49b18bf09f28
+data "aws_iam_policy_document" "cluster_autoscaler" {
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DescribeTags",
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "ec2:DescribeLaunchTemplateVersions"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "cwlogs" {
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:CreateLogGroup",
+      "logs:DescribeLogStreams",
+      "logs:PutLogEvents",
+    ]
+  }
+}
