53602fe274732e36bd9662dfc97ba330010af688
Vara Bonthu <vara.bonthu@gmail.com>
2021-12-08 13:04:32 +0000

    Added terrascan github actions; updated workflows

--- .github/test/e2e/main.tf
+++ .github/test/e2e/main.tf
@@ -18,15 +18,17 @@ terraform {
   }
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-ssp-github-actions-state"
+    key    = "tf-state"
+    region = "us-west-2"
+  }
+}
+
 provider "aws" {
   region = data.aws_region.current.id
   alias  = "default"
-}
-
-terraform {
-  backend "local" {
-    path = "local_tf_state/terraform-main.tfstate"
-  }
 }
 
 data "aws_region" "current" {}
@@ -78,7 +80,7 @@ module "aws_vpc" {
 # Example to consume aws-eks-accelerator-for-terraform module
 #---------------------------------------------------------------
 module "aws-eks-accelerator-for-terraform" {
-  source = "../.."
+  source = "../../.."
 
   tenant            = local.tenant
   environment       = local.environment
