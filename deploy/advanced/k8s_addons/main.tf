6b63dfc873ec16ce481a11baa3aef57a66cf7177
Vara Bonthu <vara.bonthu@gmail.com>
2021-12-24 12:13:04 +0000

    source updates for k8s addons module

--- deploy/advanced/k8s_addons/main.tf
+++ deploy/advanced/k8s_addons/main.tf
@@ -154,7 +154,7 @@ module "aws-eks-accelerator-for-terraform" {
 }
 
 module "kubernetes-addons" {
-  source         = "github.com/aws-samples/aws-eks-accelerator-for-terraform//modules/kubernetes-addons"
+  source         = "../../../modules/kubernetes-addons"
 
   eks_cluster_id               = module.aws-eks-accelerator-for-terraform.eks_cluster_id
   eks_oidc_issuer_url          = module.aws-eks-accelerator-for-terraform.eks_oidc_issuer_url
