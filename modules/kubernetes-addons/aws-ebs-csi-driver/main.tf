62e410e0b7266b982f929c77d395fa40cc5a3b00
Kevin Coleman <kcaws@amazon.com>
2021-12-28 13:52:50 -0800

    Preventing creation of ebs service account

--- modules/kubernetes-addons/aws-ebs-csi-driver/main.tf
+++ modules/kubernetes-addons/aws-ebs-csi-driver/main.tf
@@ -23,25 +23,25 @@ resource "aws_eks_addon" "aws_ebs_csi_driver" {
   resolve_conflicts        = local.add_on_config["resolve_conflicts"]
   service_account_role_arn = local.add_on_config["service_account_role_arn"] == "" ? module.irsa_addon.irsa_iam_role_arn : local.add_on_config["service_account_role_arn"]
   tags = merge(
-    var.common_tags, local.add_on_config["tags"],
+    var.common_tags, 
+    local.add_on_config["tags"],
     { "eks_addon" = "aws-ebs-csi-driver" }
   )
-
   depends_on = [module.irsa_addon]
 }
 
 module "irsa_addon" {
-  source                      = "../../../modules/irsa"
-  eks_cluster_id              = var.eks_cluster_id
-  create_kubernetes_namespace = false
-  kubernetes_namespace        = local.add_on_config["namespace"]
-  kubernetes_service_account  = local.add_on_config["service_account"]
-  irsa_iam_policies           = concat([aws_iam_policy.aws_ebs_csi_driver.arn], local.add_on_config["additional_iam_policies"])
-  tags                        = var.common_tags
+  source                            = "../../../modules/irsa"
+  create_kubernetes_namespace       = false
+  create_kubernetes_service_account = false
+  eks_cluster_id                    = var.eks_cluster_id
+  kubernetes_namespace              = local.add_on_config["namespace"]
+  kubernetes_service_account        = local.add_on_config["service_account"]
+  irsa_iam_policies                 = concat([aws_iam_policy.aws_ebs_csi_driver.arn], local.add_on_config["additional_iam_policies"])
+  tags                              = var.common_tags
 }
 
 resource "aws_iam_policy" "aws_ebs_csi_driver" {
-
   description = "IAM Policy for AWS EBS CSI Driver"
   name        = "${var.eks_cluster_id}-${local.add_on_config["addon_name"]}-irsa"
   path        = var.iam_role_path
