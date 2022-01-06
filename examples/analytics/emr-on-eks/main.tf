cd04ba0035940fa9b1318b24c51f623c3a2a98cd
Vara Bonthu <vara.bonthu@gmail.com>
2022-01-06 15:44:58 +0000

    Changed source to https path

--- examples/analytics/emr-on-eks/main.tf
+++ examples/analytics/emr-on-eks/main.tf
@@ -98,7 +98,7 @@ module "aws_vpc" {
 # Example to consume aws-eks-accelerator-for-terraform module
 #---------------------------------------------------------------
 module "aws-eks-accelerator-for-terraform" {
-  source = "../../.."
+  source = "github.com/aws-samples/aws-eks-accelerator-for-terraform"
 
   create_eks = true
 
@@ -151,7 +151,7 @@ module "aws-eks-accelerator-for-terraform" {
 }
 
 module "kubernetes-addons" {
-  source = "../../../modules/kubernetes-addons"
+  source = "github.com/aws-samples/aws-eks-accelerator-for-terraform//modules/kubernetes-addons"
 
   eks_cluster_id = module.aws-eks-accelerator-for-terraform.eks_cluster_id
 
