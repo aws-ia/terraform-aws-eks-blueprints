b519d244d95e5d56a6d5af3a0bb7a52130f2a73d
Vara Bonthu <vara.bonthu@gmail.com>
2022-01-11 19:27:52 +0000

    Added additional policy to Karpenter to restrcit IAM to delete only Karpenter nodes

--- modules/kubernetes-addons/karpenter/data.tf
+++ modules/kubernetes-addons/karpenter/data.tf
@@ -6,6 +6,7 @@ data "aws_eks_cluster" "eks" {
 
 data "aws_iam_policy_document" "karpenter" {
   statement {
+    sid       = "Karpenter"
     effect    = "Allow"
     resources = ["*"]
 
@@ -15,7 +16,6 @@ data "aws_iam_policy_document" "karpenter" {
       "ec2:RunInstances",
       "ec2:CreateTags",
       "iam:PassRole",
-      "ec2:TerminateInstances",
       "ec2:DescribeLaunchTemplates",
       "ec2:DescribeInstances",
       "ec2:DescribeSecurityGroups",
@@ -25,5 +25,18 @@ data "aws_iam_policy_document" "karpenter" {
       "ec2:DescribeAvailabilityZones",
       "ssm:GetParameter"
     ]
+  }
+
+  statement {
+    sid       = "ConditionalEC2Termination"
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["ec2:TerminateInstances"]
+
+    condition {
+      test     = "StringLike"
+      variable = "ec2:ResourceTag/Name"
+      values   = ["*karpenter*"]
+    }
   }
 }
