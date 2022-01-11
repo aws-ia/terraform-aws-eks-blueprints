2c7fa41f55cfe8ecabcbfb0695bd2f7c6dbe1a5f
Waleed Sarwari <sarwaw@amazon.com>
2022-01-11 14:03:59 -0500

    undo the aws_region resource delete

--- examples/1-eks-cluster-with-new-vpc/main.tf
+++ examples/1-eks-cluster-with-new-vpc/main.tf
@@ -21,9 +21,9 @@ terraform {
   }
 }
 
-data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
 
-data "aws_caller_identity" "current" {}
+data "aws_availability_zones" "available" {}
 
 data "aws_eks_cluster" "cluster" {
   name = module.aws-eks-accelerator-for-terraform.eks_cluster_id
