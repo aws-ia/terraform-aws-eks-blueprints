2c00fed8a3cd00da6418da8e01be74617d74067b
Vara Bonthu <vara.bonthu@gmail.com>
2022-02-11 16:20:14 +0000

    removed kubetcl version from kuberentes addon module

--- modules/kubernetes-addons/versions.tf
+++ modules/kubernetes-addons/versions.tf
@@ -38,10 +38,6 @@ terraform {
       source  = "hashicorp/null"
       version = "3.1.0"
     }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.13.1"
-    }
   }
   required_version = ">= 1.0.0"
 }
