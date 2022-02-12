c092b450b47d9d342825e5c321dde7c0a8bf9c1a
Vara Bonthu <vara.bonthu@gmail.com>
2022-02-12 13:48:15 +0000

    Added terrafrom versions for example

--- examples/analytics/emr-on-eks/main.tf
+++ examples/analytics/emr-on-eks/main.tf
@@ -1,3 +1,26 @@
+terraform {
+  required_version = ">= 1.0.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.66.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.6.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
+  }
+
+  backend "local" {
+    path = "local_tf_state/terraform-main.tfstate"
+  }
+}
+
 provider "aws" {}
 
 provider "kubernetes" {
@@ -27,12 +50,6 @@ data "aws_eks_cluster" "cluster" {
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.aws-eks-accelerator-for-terraform.eks_cluster_id
-}
-
-terraform {
-  backend "local" {
-    path = "local_tf_state/terraform-main.tfstate"
-  }
 }
 
 locals {
