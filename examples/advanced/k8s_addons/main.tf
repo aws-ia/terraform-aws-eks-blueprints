31488b31bd84e4df8cbc35ed08d681b74d72bc0f
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-02-23 07:22:49 -0800

    change create eks default (#278)

--- examples/advanced/k8s_addons/main.tf
+++ examples/advanced/k8s_addons/main.tf
@@ -85,7 +85,6 @@ module "aws-eks-accelerator-for-terraform" {
   private_subnet_ids = local.private_subnet_ids
 
   # EKS CONTROL PLANE VARIABLES
-  create_eks         = true
   kubernetes_version = local.kubernetes_version
 
   # EKS MANAGED NODE GROUPS
