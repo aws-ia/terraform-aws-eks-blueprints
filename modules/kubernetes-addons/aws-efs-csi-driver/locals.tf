59bdedebe409d49f614f9c7b6a8304ae196d7206
Luigi Di Fraia <luigi.difraia@nominet.uk>
2022-02-28 22:10:15 +0000

    Remove Helm config items that match defaults; also use default timeout

--- modules/kubernetes-addons/aws-efs-csi-driver/locals.tf
+++ modules/kubernetes-addons/aws-efs-csi-driver/locals.tf
@@ -9,36 +9,8 @@ locals {
     repository                 = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
     version                    = "2.2.3"
     namespace                  = local.namespace
-    timeout                    = "1200"
-    create_namespace           = false
     values                     = local.default_helm_values
-    set                        = []
-    set_sensitive              = null
-    lint                       = true
-    wait                       = true
-    wait_for_jobs              = false
     description                = "The AWS EFS CSI driver Helm chart deployment configuration"
-    verify                     = false
-    keyring                    = ""
-    repository_key_file        = ""
-    repository_cert_file       = ""
-    repository_ca_file         = ""
-    repository_username        = ""
-    repository_password        = ""
-    disable_webhooks           = false
-    reuse_values               = false
-    reset_values               = false
-    force_update               = false
-    recreate_pods              = false
-    cleanup_on_fail            = false
-    max_history                = 0
-    atomic                     = false
-    skip_crds                  = false
-    render_subchart_notes      = true
-    disable_openapi_validation = false
-    dependency_update          = false
-    replace                    = false
-    postrender                 = ""
   }
 
   helm_config = merge(
