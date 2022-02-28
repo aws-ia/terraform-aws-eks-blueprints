4047ca7c52ecc7e56078b73f45a174ff7ede9410
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-02-28 15:00:38 -0800

    fix irsa_config

--- modules/kubernetes-addons/kubernetes-dashboard/locals.tf
+++ modules/kubernetes-addons/kubernetes-dashboard/locals.tf
@@ -34,7 +34,7 @@ locals {
   ]
 
   irsa_config = {
-    kubernetes_namespace              = local.name
+    kubernetes_namespace              = local.namespace
     kubernetes_service_account        = local.service_account_name
     create_kubernetes_namespace       = false
     create_kubernetes_service_account = true
