14c4d01aa62678c994ba80f7458d518a729d89ad
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-03-03 20:59:08 -0800

    fix var name

--- modules/kubernetes-addons/main.tf
+++ modules/kubernetes-addons/main.tf
@@ -197,7 +197,7 @@ module "prometheus" {
 module "spark-k8s-operator" {
   count             = var.enable_vpa ? 1 : 0
   source            = "./spark-k8s-operator"
-  helm_config       = var.spark-k8s-operator_helm_config
+  helm_config       = var.spark_k8s_operator_helm_config
   manage_via_gitops = var.argocd_manage_add_ons
   addon_context     = local.addon_context
 }
