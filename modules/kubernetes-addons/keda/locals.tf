e5dfb6b4fde56f5e53dbdc5baea285f4d14036f7
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-03-04 01:46:41 -0800

    refactor keda for helm-addon (#312)
    * refactor keda for helm-addon
    
    * terraform-docs: automated action
    
    * do not create sa via helm values
    
    * flag for create_irsa
    
    * terraform-docs: automated action
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/kubernetes-addons/keda/locals.tf
+++ modules/kubernetes-addons/keda/locals.tf
@@ -1,80 +1,45 @@
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: MIT-0
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify,
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 locals {
-  service_account_name = "keda-operator"
-  namespace            = "keda-ns"
-
-  irsa_set_values = [{
-    name  = "serviceAccount.create"
-    value = "false"
-    },
-    {
-      name  = "serviceAccount.name"
-      value = local.service_account_name
-  }]
-
+  name                 = "keda"
+  service_account_name = "keda-operator-sa"
   default_helm_config = {
-    name                       = "keda"
-    chart                      = "keda"
-    repository                 = "https://kedacore.github.io/charts"
-    version                    = "2.6.2"
-    namespace                  = "keda"
-    timeout                    = "1200"
-    create_namespace           = false
-    description                = "Keda Event-based autoscaler for workloads on Kubernetes"
-    lint                       = false
-    wait                       = true
-    wait_for_jobs              = false
-    verify                     = false
-    keyring                    = ""
-    repository_key_file        = ""
-    repository_cert_file       = ""
-    repository_ca_file         = ""
-    repository_username        = ""
-    repository_password        = ""
-    disable_webhooks           = false
-    reuse_values               = false
-    reset_values               = false
-    force_update               = false
-    recreate_pods              = false
-    cleanup_on_fail            = false
-    max_history                = 0
-    atomic                     = false
-    skip_crds                  = false
-    render_subchart_notes      = true
-    disable_openapi_validation = false
-    dependency_update          = false
-    replace                    = false
-    postrender                 = ""
-    set                        = []
-    set_sensitive              = []
-    values                     = local.default_helm_values
+    name        = local.name
+    chart       = local.name
+    repository  = "https://kedacore.github.io/charts"
+    version     = "2.6.2"
+    namespace   = local.name
+    description = "Keda Event-based autoscaler for workloads on Kubernetes"
+    values      = local.default_helm_values
+    timeout     = "1200"
   }
+
+  default_helm_values = [templatefile("${path.module}/values.yaml", {
+    sa-name = local.service_account_name
+  })]
 
   helm_config = merge(
     local.default_helm_config,
     var.helm_config
   )
 
-  default_helm_values = [templatefile("${path.module}/values.yaml", {
-    keda-sa-name = local.service_account_name
-  })]
+  set_values = [
+    {
+      name  = "serviceAccount.name"
+      value = local.service_account_name
+    },
+    {
+      name  = "serviceAccount.create"
+      value = false
+    }
+  ]
+
+  irsa_config = {
+    kubernetes_namespace              = local.namespace
+    kubernetes_service_account        = local.service_account_name
+    create_kubernetes_namespace       = true
+    create_kubernetes_service_account = true
+    irsa_iam_policies                 = concat([aws_iam_policy.keda_irsa[0].arn], var.irsa_policies)
+    irsa_iam_permissions_boundary     = var.irsa_permissions_boundary
+  }
 
   argocd_gitops_config = {
     enable             = true
