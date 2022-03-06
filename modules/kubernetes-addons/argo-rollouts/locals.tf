a781ba42803a79f609689ed65eb80d478764b6e9
Vara Bonthu <vara.bonthu@gmail.com>
2022-03-06 20:01:20 +0000

    Argo rollout update

--- modules/kubernetes-addons/argo-rollouts/locals.tf
+++ modules/kubernetes-addons/argo-rollouts/locals.tf
@@ -1,6 +1,6 @@
 locals {
   name                 = "argo-rollouts"
-  service_account_name = "${local.name}-sa"
+
   default_helm_config = {
     name        = local.name
     chart       = local.name
@@ -8,44 +8,16 @@ locals {
     version     = "2.9.1"
     namespace   = local.name
     description = "Argo Rollouts AddOn Helm Chart"
-    values      = local.default_helm_values
+    values      = null
     timeout     = "1200"
   }
-
-  default_helm_values = [templatefile("${path.module}/values.yaml", {
-    sa-name = local.service_account_name
-  })]
 
   helm_config = merge(
     local.default_helm_config,
     var.helm_config
   )
 
-  set_values = [
-    {
-      name  = "serviceAccount.name"
-      value = local.service_account_name
-    },
-    {
-      name  = "serviceAccount.create"
-      value = false
-    }
-  ]
-
-  irsa_config = {
-    kubernetes_namespace              = local.name
-    kubernetes_service_account        = local.service_account_name
-    create_kubernetes_namespace       = true
-    create_kubernetes_service_account = true
-    iam_role_path                     = "/"
-    tags                              = var.addon_context.tags
-    eks_cluster_id                    = var.addon_context.eks_cluster_id
-    irsa_iam_policies                 = []
-    irsa_iam_permissions_boundary     = var.irsa_iam_permissions_boundary
-  }
-
   argocd_gitops_config = {
     enable             = true
-    serviceAccountName = local.service_account_name
   }
 }
