9828bee72f67218e71e6c854a7caad95cfd59a62
Rodrigue Koffi <koffir@amazon.com>
2022-03-09 18:26:00 +0000

    Remove dead variables

--- modules/kubernetes-addons/aws-opentelemetry-eks-jmx/locals.tf
+++ modules/kubernetes-addons/aws-opentelemetry-eks-jmx/locals.tf
@@ -8,7 +8,6 @@ locals {
     create_namespace           = true
     description                = "ADOT helm Chart deployment configuration"
     lint                       = false
-    values                     = local.default_helm_values
     values                     = []
     wait                       = true
     wait_for_jobs              = false
@@ -42,28 +41,18 @@ locals {
     var.helm_config
   )
 
-  default_helm_values = [templatefile("${path.module}/otel-config/values.yaml", {
-    amp_url = "${var.otel_config.amazon_prometheus_remote_write_url}api/v1/remote_write",
-    region  = var.otel_config.amazon_prometheus_region
-  })]
-
-  /*
-  amazon_prometheus_workspace_url          = var.amazon_prometheus_workspace_endpoint != null ? "${var.amazon_prometheus_workspace_endpoint}api/v1/remote_write" : ""
-  */
   amazon_prometheus_ingest_service_account = "amp-ingest"
-  amazon_prometheus_ingest_iam_role_arn    = (var.amazon_prometheus_remote_write_url != null) ? module.irsa_amp_ingest[0].irsa_iam_role_arn : ""
+  amazon_prometheus_ingest_iam_role_arn    = (var.otel_config.amazon_prometheus_remote_write_url != null) ? module.irsa_amp_ingest[0].irsa_iam_role_arn : ""
 
-  /*
   amp_config_values = [
     {
       name  = "ampurl"
-      value = local.amp_url["amp_url"]
+      value = "${var.otel_config.amazon_prometheus_remote_write_url}api/v1/remote_write"
     },
     {
       name  = "region"
-      value = local.amp_url["region"]
+      value = var.otel_config.amazon_prometheus_region
     },
   ]
-  */
 
 }
