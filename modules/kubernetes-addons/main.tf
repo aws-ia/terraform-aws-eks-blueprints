1158718939b2a7319ebc5dbbcc11373eb72b06bd
Yaroslav Skopets <yaroslav@tetrate.io>
2022-03-17 18:26:49 +0100

    code review: use stable dependency
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>

--- modules/kubernetes-addons/main.tf
+++ modules/kubernetes-addons/main.tf
@@ -210,7 +210,7 @@ module "spark_k8s_operator" {
 module "tetrate_istio" {
   count                = var.enable_tetrate_istio ? 1 : 0
   source               = "tetratelabs/tetrate-istio-addon/eksblueprints"
-  version              = "0.0.2"
+  version              = "0.0.3"
   distribution         = var.tetrate_istio_distribution
   distribution_version = var.tetrate_istio_version
   install_base         = var.tetrate_istio_install_base
