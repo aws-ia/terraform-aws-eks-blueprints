e516da5296428b664c5079dbaad3f625f16e3d51
Vara Bonthu <vara.bonthu@gmail.com>
2022-03-22 08:28:16 +0000

    managed ndoe group default set to m5

--- modules/aws-eks-managed-node-groups/locals.tf
+++ modules/aws-eks-managed-node-groups/locals.tf
@@ -1,9 +1,9 @@
 
 locals {
   default_managed_ng = {
-    node_group_name          = "m4_on_demand" # Max node group length is 40 characters; including the node_group_name_prefix random id it's 63
+    node_group_name          = "m5_on_demand" # Max node group length is 40 characters; including the node_group_name_prefix random id it's 63
     enable_node_group_prefix = true
-    instance_types           = ["m4.large"]
+    instance_types           = ["m5.large"]
     capacity_type            = "ON_DEMAND"  # ON_DEMAND, SPOT
     ami_type                 = "AL2_x86_64" # AL2_x86_64, AL2_x86_64_GPU, AL2_ARM_64, BOTTLEROCKET_x86_64, BOTTLEROCKET_ARM_64
     custom_ami_id            = ""
