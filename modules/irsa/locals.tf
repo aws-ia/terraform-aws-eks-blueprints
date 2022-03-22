7d299d61ec0ed584fbe1e921c5e0eb2f86175c8f
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-03-22 09:43:55 -0700

    removing default values

--- modules/irsa/locals.tf
+++ modules/irsa/locals.tf
@@ -1,10 +1,3 @@
 terraform {
   experiments = [module_variable_optional_attrs]
 }
-
-locals {
-  addon_context = defaults(var.addon_context, {
-    irsa_iam_role_path            = "/"
-    irsa_iam_permissions_boundary = ""
-  })
-}
