3f6ce8ab5fee8104c5b613c86a79f6d01f318973
Naris Silpakit <silpn@amazon.com>
2022-03-22 18:10:00 -0700

    add output with cli command for adding new cluster to kubeconfig to all examples

--- examples/aws-efs-csi-driver/main.tf
+++ examples/aws-efs-csi-driver/main.tf
@@ -199,3 +199,8 @@ output "efs_file_system_id" {
   description = "ID of the EFS file system to use for creating a storage class"
   value       = aws_efs_file_system.efs.id
 }
+
+output "configure_kubectl" {
+  description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value       = "aws eks --region ${data.aws_region.current.id} update-kubeconfig --name ${module.aws-eks-accelerator-for-terraform.eks_cluster_id}"
+}
