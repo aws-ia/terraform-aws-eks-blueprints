c15c3848b2b67089b9fb5a3b0bc5187a01801566
Naris Silpakit <silpn@amazon.com>
2022-03-22 18:10:00 -0700

    add output with cli command for adding new cluster to kubeconfig to all examples

--- examples/karpenter/main.tf
+++ examples/karpenter/main.tf
@@ -208,3 +208,8 @@ resource "kubectl_manifest" "karpenter_provisioner" {
 
   depends_on = [module.kubernetes-addons]
 }
+
+output "configure_kubectl" {
+  description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value       = "aws eks --region ${data.aws_region.current.id} update-kubeconfig --name ${module.aws-eks-accelerator-for-terraform.eks_cluster_id}"
+}
--- examples/observability/eks-cluster-with-amp-amg-opensearch/outputs.tf
+++ examples/observability/eks-cluster-with-amp-amg-opensearch/outputs.tf
@@ -17,3 +17,8 @@ output "opensearch_vpc_endpoint" {
 output "bastion_host_public_ip" {
   value = aws_instance.bastion_host.public_ip
 }
+
+output "configure_kubectl" {
+  description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value       = "aws eks --region ${data.aws_region.current.id} update-kubeconfig --name ${module.aws-eks-accelerator-for-terraform.eks_cluster_id}"
+}
