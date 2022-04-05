4b1a4e9e6d0cd949cb4d5c361d9db94f135b2338
greengangsta <acc.surajtripathi@gmail.com>
2022-04-06 00:37:10 +0530

    updated s3 kms key settings

--- modules/kubernetes-addons/velero/main.tf
+++ modules/kubernetes-addons/velero/main.tf
@@ -26,7 +26,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.mykey.arn
       sse_algorithm     = "AES256"
     }
   }
