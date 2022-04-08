54f2e299e7e9c0e3d7b09c77f2559777aea365ee
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-04-07 23:19:49 -0700

    remove newlines

--- modules/kubernetes-addons/metrics-server/main.tf
+++ modules/kubernetes-addons/metrics-server/main.tf
@@ -9,12 +9,10 @@ module "helm_addon" {
 }
 
 resource "kubernetes_namespace_v1" "this" {
-
   count = local.helm_config["namespace"] == "kube-system" ? 0 : 1
 
   metadata {
     name = local.helm_config["namespace"]
-
     labels = {
       "app.kubernetes.io/managed-by" = "terraform-eks-blueprints"
     }
