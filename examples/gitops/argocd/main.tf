4826bcb4f6236a8a423136c508c401450f3d5968
camba1 <peredo01@gmail.com>
2022-04-10 18:39:00 -0500

    reset load balancer

--- examples/gitops/argocd/main.tf
+++ examples/gitops/argocd/main.tf
@@ -175,7 +175,7 @@ module "eks-blueprints-kubernetes-addons" {
   #---------------------------------------------------------------
 
   enable_aws_for_fluentbit            = true
-  enable_aws_load_balancer_controller = false
+  enable_aws_load_balancer_controller = true
   enable_cert_manager                 = true
   enable_cluster_autoscaler           = true
   enable_ingress_nginx                = true
