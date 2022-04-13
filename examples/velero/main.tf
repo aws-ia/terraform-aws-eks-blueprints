b8b4e1ba5c8afa92bbe6b1efe27c40fde3963780
greengangsta <acc.surajtripathi@gmail.com>
2022-04-13 18:41:06 +0530

    fixed issues in main.tf

--- examples/velero/main.tf
+++ examples/velero/main.tf
@@ -61,7 +61,7 @@ provider "helm" {
 
 provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.awkus_eks_cluster.cluster.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
   apply_retry_count      = 10
