91dc3651d33049029f7b4188b515ab16a1b11a24
Sean P. Kane <spkane00@gmail.com>
2022-04-15 09:27:50 -0700

    KMS - Additional key admins support (#414)
    > I'm assuming that many of these jobs are failing because I am using a fork and some of the required secrets for the Github Actions aren't available in this case. Let me know if you need/want me to do anything differently.

--- data.tf
+++ data.tf
@@ -94,10 +94,13 @@ data "aws_iam_policy_document" "eks_key" {
 
     principals {
       type = "AWS"
-      identifiers = [
-        "arn:${local.context.aws_partition_id}:iam::${local.context.aws_caller_identity_account_id}:role/${local.cluster_iam_role_name}",
-        data.aws_iam_session_context.current.issuer_arn
-      ]
+      identifiers = concat(
+        var.cluster_kms_key_additional_admin_arns,
+        [
+          "arn:${local.context.aws_partition_id}:iam::${local.context.aws_caller_identity_account_id}:role/${local.cluster_iam_role_name}",
+          data.aws_iam_session_context.current.issuer_arn
+        ]
+      )
     }
   }
 
--- variables.tf
+++ variables.tf
@@ -134,6 +134,12 @@ variable "cluster_kms_key_deletion_window_in_days" {
   description = "The waiting period, specified in number of days (7 - 30). After the waiting period ends, AWS KMS deletes the KMS key"
 }
 
+variable "cluster_kms_key_additional_admin_arns" {
+  type = list(string)
+  description = "A list of additional IAM ARNs that should have FULL access (kms:*) in the KMS key policy."
+  default = []
+}
+
 variable "cluster_encryption_config" {
   description = "Configuration block with encryption configuration for the cluster"
   type = list(object({
