a86264f1bfac32e2ff9478b03c9995b38bd29b5b
Kevin Coleman <kcaws@amazon.com>
2022-04-17 22:09:31 -0700

    Removing output

--- examples/eks-cluster-with-new-vpc/main.tf
+++ examples/eks-cluster-with-new-vpc/main.tf
@@ -138,8 +138,3 @@ module "eks_blueprints_kubernetes_addons" {
 
   depends_on = [module.eks_blueprints.managed_node_groups]
 }
-
-output "configure_kubectl" {
-  description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = module.eks_blueprints.configure_kubectl
-}
