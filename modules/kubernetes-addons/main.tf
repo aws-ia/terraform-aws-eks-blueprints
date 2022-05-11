42c0ef8c0e317ac790d7b3de437f66b032409fb0
awsvikram <vikramv1989@gmail.com>
2022-05-11 10:37:25 -0400

    Update main.tf

--- modules/kubernetes-addons/main.tf
+++ modules/kubernetes-addons/main.tf
@@ -316,15 +316,6 @@ module "adot_collector_nginx" {
   addon_context                        = local.addon_context
 }
 
-module "adot_collector_nginx" {
-  count                                = var.enable_adot_collector_nginx ? 1 : 0
-  source                               = "./adot-collector-nginx"
-  helm_config                          = var.adot_collector_nginx_helm_config
-  amazon_prometheus_workspace_endpoint = var.amazon_prometheus_workspace_endpoint
-  amazon_prometheus_workspace_region   = var.amazon_prometheus_workspace_region
-  addon_context                        = local.addon_context
-}
-
 module "adot_collector_haproxy" {
   count                                = var.enable_adot_collector_haproxy ? 1 : 0
   source                               = "./adot-collector-haproxy"
