9f04d91bcf12b18f3757f4a7de98d4db1fc1a8db
Ramesh Kumar Venkatraman <rameshkumar.venkatraman@gmail.com>
2022-05-13 14:44:51 +0000

    added managed prometheus alert manager config for java/jmx modules. Added additional output variable to get the managed prometheus workspace id

--- examples/observability/adot-amp-grafana-for-java/main.tf
+++ examples/observability/adot-amp-grafana-for-java/main.tf
@@ -144,3 +144,38 @@ resource "grafana_dashboard" "jmx_dashboards" {
   folder      = grafana_folder.jmx_dashboards.id
   config_json = file("${path.module}/dashboards/default.json")
 }
+
+#Configure AWS Managed Prometheus recording and alerting rules
+resource "aws_prometheus_rule_group_namespace" "java_jmx" {
+  name         = "java_jmx_rules"
+  workspace_id = module.eks_blueprints.amazon_prometheus_workspace_id
+  data         = <<EOF
+groups:
+  - name: default-metric
+    rules:
+    - record: metric:recording_rule
+      expr: avg(rate(container_cpu_usage_seconds_total[5m]))
+  - name: default-alert
+    rules:
+    - alert: metric:alerting_rule
+      expr: jvm_memory_bytes_used{job="java", area="heap"} / jvm_memory_bytes_max * 100 > 80
+      for: 1m
+      labels:
+          severity: warning
+      annotations:
+          summary: "JVM heap warning"
+          description: "JVM heap of instance `{{$labels.instance}}` from application `{{$labels.application}}` is above 80% for one minute. (current=`{{$value}}%`)"
+EOF
+}
+
+#Configure AWS Managed Prometheus alert manager
+resource "aws_prometheus_alert_manager_definition" "java_jmx" {
+  workspace_id = module.eks_blueprints.amazon_prometheus_workspace_id
+  definition   = <<EOF
+alertmanager_config: |
+  route:
+    receiver: 'default'
+  receivers:
+    - name: 'default'
+EOF
+}
