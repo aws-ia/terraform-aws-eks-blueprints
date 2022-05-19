1dae0d95df775d2255a84cdde67bd0fcc966ad20
Bryant Biggs <bryantbiggs@gmail.com>
2022-05-19 03:33:13 -0400

    fix: Correct pre-commit failures and e2e apply/destroy errors (#566)

--- modules/kubernetes-addons/cert-manager/versions.tf
+++ modules/kubernetes-addons/cert-manager/versions.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.10"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.4.1"
