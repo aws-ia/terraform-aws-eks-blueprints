19457d04d7356786c8f706294277c5f8e29ff663
Vara Bonthu <vara.bonthu@gmail.com>
2022-05-31 22:54:24 +0100

    removed dupliate policy

--- examples/analytics/emr-on-eks/main.tf
+++ examples/analytics/emr-on-eks/main.tf
@@ -28,34 +28,6 @@ provider "helm" {
   }
 }
 
-data "aws_iam_policy_document" "emr_on_eks" {
-  statement {
-    sid       = ""
-    effect    = "Allow"
-    resources = ["arn:${data.aws_partition.current.partition}:s3:::*"]
-
-    actions = [
-      "s3:PutObject",
-      "s3:GetObject",
-      "s3:ListBucket",
-      "s3:DeleteObject",
-      "s3:DeleteObjectVersion"
-    ]
-  }
-
-  statement {
-    sid       = ""
-    effect    = "Allow"
-    resources = ["arn:${data.aws_partition.current.partition}:logs:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:log-group:*"]
-
-    actions = [
-      "logs:PutLogEvents",
-      "logs:CreateLogStream",
-      "logs:DescribeLogGroups",
-      "logs:DescribeLogStreams",
-    ]
-  }
-}
 data "aws_availability_zones" "available" {}
 
 locals {
