4b0cbc5d559204c1f870132e842cee83fcaba9ee
Zvikan <zvi8875@gmail.com>
2022-06-01 10:46:33 -0500

    fix: use correct var names

--- modules/kubernetes-addons/locals.tf
+++ modules/kubernetes-addons/locals.tf
@@ -25,12 +25,12 @@ locals {
     externalDns               = var.enable_external_dns ? module.external_dns[0].argocd_gitops_config : null
   }
 
-  eks_oidc_issuer_url = var.eks_oidc_issuer_url
+  eks_oidc_issuer_url = var.eks_oidc_provider
 
   addon_context = {
     aws_caller_identity_account_id = data.aws_caller_identity.current.account_id
     aws_caller_identity_arn        = data.aws_caller_identity.current.arn
-    aws_eks_cluster_endpoint       = var.aws_eks_cluster_endpoint
+    aws_eks_cluster_endpoint       = var.eks_cluster_endpoint
     aws_partition_id               = data.aws_partition.current.partition
     aws_region_name                = data.aws_region.current.name
     eks_cluster_id                 = var.eks_cluster_id
