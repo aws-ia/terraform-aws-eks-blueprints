099136c975f874a1daad7ceaed15e0b0070b7862
Satveer Khurpa <khurpas@amazon.com>
2022-06-01 19:26:29 -0500

    multitenancy stack updates

--- examples/multi-tenancy-with-teams/main.tf
+++ examples/multi-tenancy-with-teams/main.tf
@@ -46,7 +46,8 @@ data "aws_caller_identity" "current" {}
 data "aws_availability_zones" "available" {}
 
 locals {
-  name   = basename(path.cwd)
+  #name   = basename(path.cwd)
+  name   = "multitenancy-eks"
   region = "us-west-2"
 
   vpc_cidr = "10.0.0.0/16"
