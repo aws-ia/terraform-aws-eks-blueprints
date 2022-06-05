00fdf44f416e4da574c1fa71015e5be8863e1286
Hokwang Lee <luckyhorang@nate.com>
2022-06-06 01:38:09 +0900

    Update versions.tf

--- modules/kubernetes-addons/kubernetes-dashboard/versions.tf
+++ modules/kubernetes-addons/kubernetes-dashboard/versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
     }
   }
 }
