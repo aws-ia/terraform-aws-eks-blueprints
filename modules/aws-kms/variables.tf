204db5a320e1a9033a269af33d4ea6bffdc258d3
Bryant Biggs <bryantbiggs@gmail.com>
2022-06-11 09:31:36 -0400

    fix: Align kms module variables

--- modules/aws-kms/variables.tf
+++ modules/aws-kms/variables.tf
@@ -14,15 +14,15 @@ variable "alias" {
 }
 
 variable "enable_key_rotation" {
-  type        = bool
   description = "Specifies whether annual key rotation is enabled"
+  type        = bool
   default     = true
 }
 
 variable "deletion_window_in_days" {
-  type        = number
   description = "The waiting period, specified in number of days. After the waiting period ends, AWS KMS deletes the KMS key. If you specify a value, it must be between 7 and 30, inclusive. If you do not specify a value, it defaults to 30"
-  default     = 30
+  type    = number
+  default = 30
 }
 
 variable "tags" {
