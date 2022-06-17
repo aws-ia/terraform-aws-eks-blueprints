2e07d4216285289c114e764a27de6dbfec0d12a3
Zvikan <zvi8875@gmail.com>
2022-06-16 22:59:48 -0500

    fix: add helm provider self-managed node example

--- examples/node-groups/self-managed-node-groups/main.tf
+++ examples/node-groups/self-managed-node-groups/main.tf
@@ -14,6 +14,20 @@ provider "kubernetes" {
   }
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_blueprints.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
+    }
+  }
+}
+
 data "aws_availability_zones" "available" {}
 
 locals {
