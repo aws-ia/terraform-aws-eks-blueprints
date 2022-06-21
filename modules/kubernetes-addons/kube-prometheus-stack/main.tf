330789b262da0f5ca9b2181df1b089046fcab913
alanty <45295374+alanty@users.noreply.github.com>
2022-06-21 12:17:19 -0700

    init helm addon

---
+++ modules/kubernetes-addons/kube-prometheus-stack/main.tf
@@ -0,0 +1,19 @@
+module "helm_addon" {
+  source            = "../helm-addon"
+#   manage_via_gitops = var.manage_via_gitops
+  helm_config       = local.helm_config
+  set_values        = local.default_helm_values
+#   irsa_config       = null
+  addon_context     = var.addon_context
+
+  depends_on = [kubernetes_namespace_v1.prometheus]
+}
+
+resource "kubernetes_namespace_v1" "prometheus" {
+  metadata {
+    name = local.helm_config["namespace"]
+    labels = {
+      "app.kubernetes.io/managed-by" = "terraform-aws-eks-blueprints"
+    }
+  }
+}
---
+++ modules/kubernetes-addons/kube-prometheus-stack/variables.tf
@@ -0,0 +1,40 @@
+variable "helm_config" {
+  type        = any
+  default     = {}
+  description = "Helm Config for kube-prometheus-stack"
+}
+
+# variable "enable_amazon_prometheus" {
+#   type        = bool
+#   default     = false
+#   description = "[not implemented] Enable AWS Managed Prometheus service"
+# }
+
+# variable "amazon_prometheus_workspace_endpoint" {
+#   type        = string
+#   default     = null
+#   description = "[not implemented] Amazon Managed Prometheus Workspace Endpoint"
+# }
+
+# variable "manage_via_gitops" {
+#   type        = bool
+#   default     = false
+#   description = "[not implemented] Determines if the add-on should be managed via GitOps."
+# }
+
+variable "addon_context" {
+  type = object({
+    aws_caller_identity_account_id = string
+    aws_caller_identity_arn        = string
+    aws_eks_cluster_endpoint       = string
+    aws_partition_id               = string
+    aws_region_name                = string
+    eks_cluster_id                 = string
+    eks_oidc_issuer_url            = string
+    eks_oidc_provider_arn          = string
+    tags                           = map(string)
+    irsa_iam_role_path             = string
+    irsa_iam_permissions_boundary  = string
+  })
+  description = "Input configuration for the addon"
+}
