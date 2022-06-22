a55ac38b8893f870a38d8279da04892040813f3b
Vara Bonthu <vara.bonthu@gmail.com>
2022-06-22 15:35:25 +0100

    EMR on EKS example with updated config

--- examples/analytics/emr-on-eks/main.tf
+++ examples/analytics/emr-on-eks/main.tf
@@ -388,47 +388,6 @@ module "vpc" {
   tags = local.tags
 }
 
-data "aws_security_group" "default" {
-  name   = "default"
-  vpc_id = module.vpc.vpc_id
-}
-
-#---------------------------------------------------------------
-# VPC Endpoints
-#---------------------------------------------------------------
-
-module "vpc_endpoints" {
-  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
-  version = "~> 3.0"
-
-  vpc_id             = module.vpc.vpc_id
-  security_group_ids = [data.aws_security_group.default.id]
-
-  endpoints = merge({
-    s3 = {
-      service      = "s3"
-      service_type = "Gateway"
-      route_table_ids = flatten([
-        module.vpc.intra_route_table_ids,
-      module.vpc.private_route_table_ids])
-      tags = {
-        Name = "${local.name}-s3"
-      }
-    }
-    },
-    { for service in toset(["autoscaling", "ecr.api", "ecr.dkr", "ec2", "ec2messages", "elasticloadbalancing", "sts", "kms", "logs", "ssm", "ssmmessages"]) :
-      replace(service, ".", "_") =>
-      {
-        service             = service
-        subnet_ids          = module.vpc.private_subnets
-        private_dns_enabled = true
-        tags                = { Name = "${local.name}-${service}" }
-      }
-  })
-
-  tags = local.tags
-}
-
 #---------------------------------------------------------------
 # Example IAM policies for EMR job execution
 #---------------------------------------------------------------
