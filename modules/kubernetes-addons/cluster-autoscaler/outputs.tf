e70fc9bfd0467a391a57dabe04f3bbd84705aa1d
Bryant Biggs <bryantbiggs@gmail.com>
2022-06-23 16:22:57 -0400

    fix: Correctly match cluster-autoscaler image version to cluster version (#684)

--- modules/kubernetes-addons/cluster-autoscaler/outputs.tf
+++ modules/kubernetes-addons/cluster-autoscaler/outputs.tf
@@ -1,4 +1,7 @@
 output "argocd_gitops_config" {
   description = "Configuration used for managing the add-on with ArgoCD"
-  value       = var.manage_via_gitops ? local.argocd_gitops_config : null
+  value = var.manage_via_gitops ? {
+    enable             = true
+    serviceAccountName = local.service_account
+  } : null
 }
