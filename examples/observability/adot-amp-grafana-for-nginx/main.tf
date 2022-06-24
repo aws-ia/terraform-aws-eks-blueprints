fdf0da765cab4092f70a01c81de998db4ea40aeb
Prithvi Reddy <47993564+preddy727@users.noreply.github.com>
2022-06-24 08:56:03 -0500

    Update main.tf (#688)

--- examples/observability/adot-amp-grafana-for-nginx/main.tf
+++ examples/observability/adot-amp-grafana-for-nginx/main.tf
@@ -159,7 +159,6 @@ module "managed_prometheus" {
     receivers:
       - name: 'default'
   EOT
-
   rule_group_namespaces = {
     nginx = {
       name = "nginx_rules"
@@ -168,33 +167,33 @@ module "managed_prometheus" {
         - name: Nginx-HTTP-4xx-error-rate
           rules:
           - alert: metric:alerting_rule
-          expr: sum(rate(nginx_http_requests_total{status=~"^4.."}[1m])) / sum(rate(nginx_http_requests_total[1m])) * 100 > 5
-          for: 1m
-          labels:
-            severity: critical
-          annotations:
-            summary: Nginx high HTTP 4xx error rate (instance {{ $labels.instance }})
-            description: "Too many HTTP requests with status 4xx (> 5%)\n  VALUE = {{ $value }}\n  LABELS = {{ $labels }}"
+            expr: sum(rate(nginx_http_requests_total{status=~"^4.."}[1m])) / sum(rate(nginx_http_requests_total[1m])) * 100 > 5
+            for: 1m
+            labels:
+             severity: critical
+            annotations:
+             summary: Nginx high HTTP 4xx error rate (instance {{ $labels.instance }})
+             description: "Too many HTTP requests with status 4xx (> 5%)\n  VALUE = {{ $value }}\n  LABELS = {{ $labels }}"
         - name: Nginx-HTTP-5xx-error-rate
           rules:
           - alert: metric:alerting_rule
-          expr: sum(rate(nginx_http_requests_total{status=~"^5.."}[1m])) / sum(rate(nginx_http_requests_total[1m])) * 100 > 5
-          for: 1m
-          labels:
-            severity: critical
-          annotations:
-            summary: Nginx high HTTP 5xx error rate (instance {{ $labels.instance }})
-            description: "Too many HTTP requests with status 5xx (> 5%)\n  VALUE = {{ $value }}\n  LABELS = {{ $labels }}"
+            expr: sum(rate(nginx_http_requests_total{status=~"^5.."}[1m])) / sum(rate(nginx_http_requests_total[1m])) * 100 > 5
+            for: 1m
+            labels:
+             severity: critical
+            annotations:
+             summary: Nginx high HTTP 5xx error rate (instance {{ $labels.instance }})
+             description: "Too many HTTP requests with status 5xx (> 5%)\n  VALUE = {{ $value }}\n  LABELS = {{ $labels }}"
         - name: Nginx-high-latency
           rules:
           - alert: metric:alerting_rule
-          expr: histogram_quantile(0.99, sum(rate(nginx_http_request_duration_seconds_bucket[2m])) by (host, node)) > 3
-          for: 2m
-          labels:
-            severity: warning
-          annotations:
-            summary: Nginx latency high (instance {{ $labels.instance }})
-            description: "Nginx p99 latency is higher than 3 seconds\n  VALUE = {{ $value }}\n  LABELS = {{ $labels }}"
+            expr: histogram_quantile(0.99, sum(rate(nginx_http_request_duration_seconds_bucket[2m])) by (host, node)) > 3
+            for: 2m
+            labels:
+             severity: warning
+            annotations:
+             summary: Nginx latency high (instance {{ $labels.instance }})
+             description: "Nginx p99 latency is higher than 3 seconds\n  VALUE = {{ $value }}\n  LABELS = {{ $labels }}"
       EOT
     }
   }
