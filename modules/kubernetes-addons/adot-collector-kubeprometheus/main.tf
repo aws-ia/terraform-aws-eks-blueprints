c417634352abed4f3bbe683a186272d16cb24387
Venkataraman <vikrvenk@88665a367666.ant.amazon.com>
2022-06-27 12:25:05 -0400

    Node exporter & KSM

--- modules/kubernetes-addons/adot-collector-kubeprometheus/main.tf
+++ modules/kubernetes-addons/adot-collector-kubeprometheus/main.tf
@@ -1,62 +1,43 @@
-locals {
-  name      = "adot-collector-kubeprometheus"
-  namespace = try(var.helm_config.namespace, local.name)
-}
+resource "helm_release" "kube_state_metrics" {
+  count            = var.enabled_kube_state_metrics ? 1 : 0
+  chart            = var.helm_chart_name_ksm
+  create_namespace = var.helm_create_namespace_ksm
+  namespace        = var.k8s_namespace_ksm
+  name             = var.helm_release_name_ksm
+  version          = var.helm_chart_version_ksm
+  repository       = var.helm_repo_url_ksm
 
-data "aws_partition" "current" {}
-
-module "helm_addon" {
-  source = "../helm-addon"
-
-  helm_config = merge(
-    {
-      name        = local.name
-      chart       = "${path.module}/otel-config"
-      version     = "0.2.0"
-      namespace   = local.namespace
-      description = "ADOT helm Chart deployment configuration"
-    },
-    var.helm_config
-  )
-
-  set_values = [
-    {
-      name  = "ampurl"
-      value = "${var.amazon_prometheus_workspace_endpoint}api/v1/remote_write"
-    },
-    {
-      name  = "region"
-      value = var.amazon_prometheus_workspace_region
-    },
-    {
-      name  = "prometheusMetricsEndpoint"
-      value = "metrics"
-    },
-    {
-      name  = "prometheusMetricsPort"
-      value = 8888
-    },
-    {
-      name  = "scrapeInterval"
-      value = "15s"
-    },
-    {
-      name  = "scrapeTimeout"
-      value = "10s"
-    },
-    {
-      name  = "scrapeSampleLimit"
-      value = 1000
-    }
+  values = [
+    var.values
   ]
 
-  irsa_config = {
-    create_kubernetes_namespace       = true
-    kubernetes_namespace              = local.namespace
-    create_kubernetes_service_account = true
-    kubernetes_service_account        = try(var.helm_config.service_account, local.name)
-    irsa_iam_policies                 = ["arn:${data.aws_partition.current.partition}:iam::aws:policy/AmazonPrometheusRemoteWriteAccess"]
+  dynamic "set" {
+    for_each = var.settings
+    content {
+      name  = set.key
+      value = set.value
+    }
   }
+}
 
-  addon_context = var.addon_context
+resource "helm_release" "prometheus_node_exporter" {
+  count            = var.enabled_node_exporter ? 1 : 0
+  chart            = var.helm_chart_name_ne
+  create_namespace = var.helm_create_namespace
+  namespace        = var.k8s_namespace_ne
+  name             = var.helm_release_name_ne
+  version          = var.helm_chart_version_ne
+  repository       = var.helm_repo_url_ne
+
+  values = [
+    var.values
+  ]
+
+  dynamic "set" {
+    for_each = var.settings
+    content {
+      name  = set.key
+      value = set.value
+    }
+  }
 }
--- modules/kubernetes-addons/adot-collector-kubeprometheus/variables.tf
+++ modules/kubernetes-addons/adot-collector-kubeprometheus/variables.tf
@@ -1,34 +1,108 @@
-variable "helm_config" {
-  description = "Helm Config for Prometheus"
-  type        = any
+
+# Kube-state-metrics Variables
+variable "enabled_kube_state_metrics" {
+  description = "Variable indicating whether deployment is enabled"
+  type        = bool
+  default     = true
+}
+
+
+# Helm
+
+variable "helm_create_namespace_ksm" {
+  type        = bool
+  default     = true
+  description = "Create the namespace if it does not yet exist"
+}
+
+variable "helm_chart_name_ksm" {
+  type        = string
+  default     = "kube-state-metrics"
+  description = "Helm chart name to be installed"
+}
+
+variable "helm_chart_version_ksm" {
+  type        = string
+  default     = "2.1.4"
+  description = "Version of the Helm chart"
+}
+
+variable "helm_release_name_ksm" {
+  type        = string
+  default     = "kube-state-metrics"
+  description = "Helm release name"
+}
+
+variable "helm_repo_url_ksm" {
+  type        = string
+  default     = "https://prometheus-community.github.io/helm-charts"
+  description = "Helm repository"
+}
+
+# K8s
+
+variable "k8s_namespace_ksm" {
+  type        = string
+  default     = "kube-system"
+  description = "The K8s namespace in which the prometheus-node-exporter service account has been created"
+}
+
+variable "settings" {
+  type        = map(any)
   default     = {}
+  description = "Additional settings which will be passed to the Helm chart values, see https://hub.helm.sh/charts/stable/prometheus-node-exporter"
 }
 
-variable "amazon_prometheus_workspace_endpoint" {
-  description = "Amazon Managed Prometheus Workspace Endpoint"
+variable "values" {
   type        = string
-  default     = null
+  default     = ""
+  description = "Additional yaml encoded values which will be passed to the Helm chart."
 }
 
-variable "amazon_prometheus_workspace_region" {
-  description = "Amazon Managed Prometheus Workspace's Region"
+# Node exporter Variables
+
+variable "enabled_node_exporter" {
+  description = "Variable indicating whether deployment is enabled"
+  type        = bool
+  default     = true
+}
+
+# Helm
+
+variable "helm_create_namespace_ne" {
+  type        = bool
+  default     = true
+  description = "Create the namespace if it does not yet exist"
+}
+
+variable "helm_chart_name_ne" {
   type        = string
-  default     = null
+  default     = "prometheus-node-exporter"
+  description = "Helm chart name to be installed"
 }
 
-variable "addon_context" {
-  description = "Input configuration for the addon"
-  type = object({
-    aws_caller_identity_account_id = string
-    aws_caller_identity_arn        = string
-    aws_eks_cluster_endpoint       = string
-    aws_partition_id               = string
-    aws_region_name                = string
-    eks_cluster_id                 = string
-    eks_oidc_issuer_url            = string
-    eks_oidc_provider_arn          = string
-    irsa_iam_permissions_boundary  = string
-    irsa_iam_role_path             = string
-    tags                           = map(string)
-  })
+variable "helm_chart_version_ne" {
+  type        = string
+  default     = "2.0.3"
+  description = "Version of the Helm chart"
+}
+
+variable "helm_release_name_ne" {
+  type        = string
+  default     = "prometheus-node-exporter"
+  description = "Helm release name"
+}
+
+variable "helm_repo_url_ne" {
+  type        = string
+  default     = "https://prometheus-community.github.io/helm-charts"
+  description = "Helm repository"
+}
+
+# K8s
+
+variable "k8s_namespace_ne" {
+  type        = string
+  default     = "kube-system"
+  description = "The K8s namespace in which the prometheus-node-exporter service account has been created"
 }
