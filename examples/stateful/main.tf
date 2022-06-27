fdce8a03e98dcc3aa947847bf93a8d5dc1fbd204
Hokwang Lee <hk.lee@hybecorp.com>
2022-06-27 22:39:01 +0900

    rollback

--- examples/stateful/main.tf
+++ examples/stateful/main.tf
@@ -71,9 +71,8 @@ module "eks_blueprints_kubernetes_addons" {
 
   eks_cluster_id = module.eks_blueprints.eks_cluster_id
 
-  enable_velero        = true
-  velero_create_bucket = true
-  velero_bucket_name   = "${local.name}-${random_string.random.result}-velero-bucket"
+  enable_velero           = true
+  velero_backup_s3_bucket = module.velero_backup_s3_bucket.s3_bucket_id
 
   tags = local.tags
 }
@@ -121,4 +120,44 @@ resource "random_string" "random" {
   length  = 16
   special = false
   upper   = false
+}
+
+#tfsec:ignore:*
+module "velero_backup_s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.0"
+
+  bucket = "${local.name}-${random_string.random.result}"
+
+  # Allow deletion of non-empty bucket
+  # NOTE: This is enabled for example usage only, you should not enable this for production workloads
+  force_destroy = true
+
+  attach_deny_insecure_transport_policy = true
+  attach_require_latest_tls_policy      = true
+
+  acl = "private"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
+
+  versioning = {
+    status     = true
+    mfa_delete = false
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = local.tags
 }
