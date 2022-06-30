7ae8a34c1a156895fff3fd8a6705241f791de279
Hokwang Lee <hk.lee@hybecorp.com>
2022-07-01 02:38:15 +0900

    fix

--- modules/aws-eks-teams/outputs.tf
+++ modules/aws-eks-teams/outputs.tf
@@ -1,14 +1,14 @@
 output "platform_teams_iam_role_arn" {
   description = "IAM role ARN for Platform Team EKS Access"
   value = tomap({
-    for k, v in aws_iam_role.platform_team : k => v.arn
+    for k, v in aws_iam_role.platform_team_access : k => v.arn
   })
 }
 
 output "application_teams_iam_role_arn" {
   description = "IAM role ARN for Application Team EKS Access"
   value = tomap({
-    for k, v in aws_iam_role.application_team : k => v.arn
+    for k, v in aws_iam_role.application_team_access : k => v.arn
   })
 }
 
@@ -29,13 +29,13 @@ output "team_sa_irsa_iam_role_arn" {
 output "platform_teams_configure_kubectl" {
   description = "Configure kubectl for each Platform Team: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
   value = tomap({
-    for k, v in aws_iam_role.platform_team : k => "aws eks --region ${data.aws_region.current.id} update-kubeconfig --name ${data.aws_eks_cluster.eks_cluster.name}  --role-arn ${v.arn}"
+    for k, v in aws_iam_role.platform_team_access : k => "aws eks --region ${data.aws_region.current.id} update-kubeconfig --name ${data.aws_eks_cluster.eks_cluster.name}  --role-arn ${v.arn}"
   })
 }
 
 output "application_teams_configure_kubectl" {
   description = "Configure kubectl for each Application Team: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
   value = tomap({
-    for k, v in aws_iam_role.application_team : k => "aws eks --region ${data.aws_region.current.id} update-kubeconfig --name ${data.aws_eks_cluster.eks_cluster.name}  --role-arn ${v.arn}"
+    for k, v in aws_iam_role.application_team_access : k => "aws eks --region ${data.aws_region.current.id} update-kubeconfig --name ${data.aws_eks_cluster.eks_cluster.name}  --role-arn ${v.arn}"
   })
 }
