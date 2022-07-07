498461a4a9a8897191fbb1e877412f3c8d22966e
Zvikan <zvi8875@gmail.com>
2022-07-07 17:24:08 -0500

    fix: TF fmt

--- examples/analytics/spark-k8s-operator/main.tf
+++ examples/analytics/spark-k8s-operator/main.tf
@@ -38,9 +38,9 @@ locals {
   name   = basename(path.cwd)
   region = "us-west-2"
 
-  vpc_cidr                           = "10.0.0.0/16"
-  azs                                = slice(data.aws_availability_zones.available.names, 0, 3)
-  s3_prefix                          = "logs/"
+  vpc_cidr  = "10.0.0.0/16"
+  azs       = slice(data.aws_availability_zones.available.names, 0, 3)
+  s3_prefix = "logs/"
 
   tags = {
     Blueprint  = local.name
