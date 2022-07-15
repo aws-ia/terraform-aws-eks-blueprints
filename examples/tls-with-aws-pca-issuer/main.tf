9bab01f6214c97c67f66e1e0e32d944cb7b41d9b
Bryant Biggs <bryantbiggs@gmail.com>
2022-07-15 11:46:34 -0400

    chore: Convert provider auth from exec() method to static token (#769)

--- examples/tls-with-aws-pca-issuer/main.tf
+++ examples/tls-with-aws-pca-issuer/main.tf
@@ -7,27 +7,19 @@ provider "kubectl" {
   host                   = module.eks_blueprints.eks_cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
   load_config_file       = false
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
-  }
+  token                  = data.aws_eks_cluster_auth.this.token
 }
 
 provider "helm" {
   kubernetes {
     host                   = module.eks_blueprints.eks_cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
-    }
+    token                  = data.aws_eks_cluster_auth.this.token
   }
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks_blueprints.eks_cluster_id
 }
 
 data "aws_partition" "current" {}
@@ -49,6 +41,7 @@ locals {
 #---------------------------------------------------------------
 # EKS Blueprints
 #---------------------------------------------------------------
+
 module "eks_blueprints" {
   source = "../.."
 
@@ -89,7 +82,6 @@ module "eks_blueprints_kubernetes_addons" {
   aws_privateca_acmca_arn     = aws_acmpca_certificate_authority.example.arn
 
   tags = local.tags
-
 }
 
 #-------------------------------
@@ -200,6 +192,7 @@ resource "kubectl_manifest" "example_pca_certificate" {
 #---------------------------------------------------------------
 # Supporting Resources
 #---------------------------------------------------------------
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 3.0"
