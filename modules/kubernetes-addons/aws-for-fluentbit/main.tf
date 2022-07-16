ca58e8a197c2d7cb87791d152ba77f02c1dd8324
Bryant Biggs <bryantbiggs@gmail.com>
2022-07-16 10:15:45 -0400

    refactor: Remove local KMS module

--- modules/kubernetes-addons/aws-for-fluentbit/main.tf
+++ modules/kubernetes-addons/aws-for-fluentbit/main.tf
@@ -22,10 +22,12 @@ resource "aws_iam_policy" "aws_for_fluent_bit" {
 }
 
 module "kms" {
-  count       = var.cw_log_group_kms_key_arn == null ? 1 : 0
-  source      = "../../../modules/aws-kms"
-  description = "EKS Workers FluentBit CloudWatch Log group KMS Key"
-  alias       = "alias/${var.addon_context.eks_cluster_id}-cw-fluent-bit"
-  policy      = data.aws_iam_policy_document.kms.json
-  tags        = var.addon_context.tags
+  source  = "terraform-aws-modules/kms/aws"
+  version = "1.0.2"
+
+  description             = "EKS Workers FluentBit CloudWatch Log group KMS Key"
+  aliases                 = ["alias/${var.addon_context.eks_cluster_id}-cw-fluent-bit"]
+  source_policy_documents = data.aws_iam_policy_document.kms.json
+
+  tags = var.addon_context.tags
 }
