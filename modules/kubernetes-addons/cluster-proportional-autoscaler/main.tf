f8eb7241a1619dc7f40ea5850f2bc060dd3e1431
kahirokunn <okinakahiro@gmail.com>
2022-07-22 14:45:29 +0900

    feat: Add kubernetes_timeouts variable to cluster-proportional-autoscaler

--- modules/kubernetes-addons/cluster-proportional-autoscaler/main.tf
+++ modules/kubernetes-addons/cluster-proportional-autoscaler/main.tf
@@ -2,10 +2,11 @@
 # Cluster Proportional Autoscaler Helm Add-on
 #-------------------------------------------------
 module "helm_addon" {
-  source            = "../helm-addon"
-  manage_via_gitops = var.manage_via_gitops
-  helm_config       = local.helm_config
-  set_values        = local.set_values
-  irsa_config       = null
-  addon_context     = var.addon_context
+  source              = "../helm-addon"
+  manage_via_gitops   = var.manage_via_gitops
+  helm_config         = local.helm_config
+  set_values          = local.set_values
+  irsa_config         = null
+  addon_context       = var.addon_context
+  kubernetes_timeouts = var.kubernetes_timeouts
 }
