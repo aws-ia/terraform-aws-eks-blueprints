cb7a2267c173a2c6893054e73b6714864b6c6574
kahirokunn <okinakahiro@gmail.com>
2022-07-22 14:45:28 +0900

    feat: Add kubernetes_timeouts variable to agones

--- modules/kubernetes-addons/agones/variables.tf
+++ modules/kubernetes-addons/agones/variables.tf
@@ -29,3 +29,9 @@ variable "addon_context" {
     tags                           = map(string)
   })
 }
+
+variable "kubernetes_timeouts" {
+  description = "Define maximum timeout for creating, updating, and deleting kubernetes resources"
+  type        = map(string)
+  default     = {}
+}
