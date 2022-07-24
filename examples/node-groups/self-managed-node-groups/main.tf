59a2d7ef5f2a6d41d682f1844debfdc50b27aac9
Vandan Juvekar <wondone@amazon.com>
2022-07-24 17:10:30 -0400

    Reverted examples

--- examples/node-groups/self-managed-node-groups/main.tf
+++ examples/node-groups/self-managed-node-groups/main.tf
@@ -24,9 +24,9 @@ data "aws_availability_zones" "available" {}
 
 locals {
   name   = basename(path.cwd)
-  region = "us-east-1"
+  region = "us-west-2"
 
-  vpc_cidr = "10.3.0.0/16"
+  vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
