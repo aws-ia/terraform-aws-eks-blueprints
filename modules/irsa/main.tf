1c8b07cb25087fa04d4f80e55e67f8f6623d0027
Bryant Biggs <bryantbiggs@gmail.com>
2022-07-27 12:38:33 -0400

    fix: Correct IRSA module name used to not default to `null` through `coalesce()` (#808)

--- modules/irsa/main.tf
+++ modules/irsa/main.tf
@@ -20,7 +20,7 @@ resource "kubernetes_service_account_v1" "irsa" {
 resource "aws_iam_role" "irsa" {
   count = var.irsa_iam_policies != null ? 1 : 0
 
-  name        = try(var.addon_context.irsa_iam_role_name, format("%s-%s-%s", var.addon_context.eks_cluster_id, trim(var.kubernetes_service_account, "-*"), "irsa"))
+  name        = try(coalesce(var.addon_context.irsa_iam_role_name, format("%s-%s-%s", var.addon_context.eks_cluster_id, trim(var.kubernetes_service_account, "-*"), "irsa")), null)
   description = "AWS IAM Role for the Kubernetes service account ${var.kubernetes_service_account}."
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
