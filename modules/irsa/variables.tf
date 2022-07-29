6d2e1b61a6705c50015073d54118381199b0d4c2
kahirokunn <okinakahiro@gmail.com>
2022-07-29 15:53:23 +0900

    chore: remove unused irsa module variables

--- modules/irsa/variables.tf
+++ modules/irsa/variables.tf
@@ -29,17 +29,12 @@ variable "irsa_iam_policies" {
 variable "addon_context" {
   description = "Input configuration for the addon"
   type = object({
-    aws_caller_identity_account_id = string
-    aws_caller_identity_arn        = string
-    aws_eks_cluster_endpoint       = string
-    aws_partition_id               = string
-    aws_region_name                = string
-    eks_cluster_id                 = string
-    eks_oidc_issuer_url            = string
-    eks_oidc_provider_arn          = string
-    tags                           = map(string)
-    irsa_iam_role_name             = optional(string)
-    irsa_iam_role_path             = optional(string)
-    irsa_iam_permissions_boundary  = optional(string)
+    eks_cluster_id                = string
+    eks_oidc_issuer_url           = string
+    eks_oidc_provider_arn         = string
+    tags                          = map(string)
+    irsa_iam_role_name            = optional(string)
+    irsa_iam_role_path            = optional(string)
+    irsa_iam_permissions_boundary = optional(string)
   })
 }
