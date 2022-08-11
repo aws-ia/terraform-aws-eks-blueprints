5158175f9bccff0408b4f272efef20095a9e014a
kahirokunn <okinakahiro@gmail.com>
2022-08-11 15:44:14 +0900

    fix: repoUrl is correct. see below.
    https://github.com/aws-samples/eks-blueprints-add-ons/blob/main/chart/values.yaml#L2

--- modules/kubernetes-addons/argocd/main.tf
+++ modules/kubernetes-addons/argocd/main.tf
@@ -60,12 +60,12 @@ resource "helm_release" "argocd_application" {
   set {
     name = "source.helm.values"
     value = each.value.add_on_application ? yamlencode(merge(
-      { repo_url = each.value.repo_url },
+      { repoUrl = each.value.repo_url },
       each.value.values,
       local.global_application_values,
       local.addon_config
       )) : yamlencode(merge(
-      { repo_url = each.value.repo_url },
+      { repoUrl = each.value.repo_url },
       each.value.values,
       local.global_application_values
     ))
