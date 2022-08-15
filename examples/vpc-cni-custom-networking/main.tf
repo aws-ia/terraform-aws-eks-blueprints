9679d272a040f288ea0a500f539cee1e8ccd4de6
Zvikan <zvi8875@gmail.com>
2022-08-15 12:28:59 -0500

    fix: remove unused data source

--- examples/vpc-cni-custom-networking/main.tf
+++ examples/vpc-cni-custom-networking/main.tf
@@ -28,7 +28,6 @@ data "aws_eks_cluster_auth" "this" {
   name = module.eks_blueprints.eks_cluster_id
 }
 
-data "aws_caller_identity" "current" {}
 data "aws_availability_zones" "available" {}
 
 locals {
