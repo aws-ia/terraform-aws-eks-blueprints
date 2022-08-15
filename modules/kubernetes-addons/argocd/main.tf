5e9f4ec717291761db0fdc51a259b20977f145cb
Vignir Hafsteinsson <vignirhafsteinsson@asana.com>
2022-08-15 14:22:00 +0000

    format fix

--- modules/kubernetes-addons/argocd/main.tf
+++ modules/kubernetes-addons/argocd/main.tf
@@ -64,14 +64,14 @@ resource "helm_release" "argocd_application" {
   }
 
   set {
-    name  = "source.helm.values"
+    name = "source.helm.values"
     value = yamlencode(merge(
       { repo_url = each.value.repo_url },
       each.value.values,
       local.global_application_values,
       each.value.add_on_application ? var.addon_config : {}
     ))
-    type  = "auto"
+    type = "auto"
   }
 
   # Destination Config.
