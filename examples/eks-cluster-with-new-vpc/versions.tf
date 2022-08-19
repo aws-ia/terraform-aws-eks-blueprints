bfebbff68f9c959a6c1d1c3b4e4f521f97fcc2cd
kahirokunn <okinakahiro@gmail.com>
2022-08-19 10:13:07 +0900

    chore: update version

--- examples/eks-cluster-with-new-vpc/versions.tf
+++ examples/eks-cluster-with-new-vpc/versions.tf
@@ -1,18 +1,18 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.2.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.72"
+      version = ">= 4.27.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.10"
+      version = ">= 2.12.1"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.4.1"
+      version = ">= 2.6.0"
     }
   }
 
