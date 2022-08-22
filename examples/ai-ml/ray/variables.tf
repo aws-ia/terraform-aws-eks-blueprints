0f8901afcd190d8c622b466dbe0b6208b97f7ed3
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-08-22 01:04:34 -0700

    fix variables.tf

--- examples/ai-ml/ray/variables.tf
+++ examples/ai-ml/ray/variables.tf
@@ -1,17 +1,17 @@
 variable "eks_cluster_domain" {
-  default     = null
-  type        = string
   description = "Optional Route53 domain for the cluster."
+  type        = string
+  default     = null
 }
 
 variable "acm_certificate_domain" {
-  default     = null
-  type        = string
   description = "Optional Route53 certificate domain"
+  type        = string
+  default     = null
 }
 
 variable "region" {
-  default     = "us-west-2"
-  type        = string
   description = "AWS Region"
+  type        = string
+  default     = "us-west-2"
 }
