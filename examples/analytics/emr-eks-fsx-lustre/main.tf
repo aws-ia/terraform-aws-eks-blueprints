e898a8fad671f3a6b0215fe04f3b3ff0ab67e28a
Vara Bonthu <vara.bonthu@gmail.com>
2022-08-22 18:27:50 +0100

    added region data source

--- examples/analytics/emr-eks-fsx-lustre/main.tf
+++ examples/analytics/emr-eks-fsx-lustre/main.tf
@@ -30,6 +30,7 @@ data "aws_eks_cluster_auth" "this" {
 data "aws_availability_zones" "available" {}
 data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
+data "aws_region" "current" {}
 
 locals {
   name   = var.name
