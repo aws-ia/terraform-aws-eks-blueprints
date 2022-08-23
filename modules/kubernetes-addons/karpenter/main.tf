a07b9b6b5b388b048304236e2b7e5c4e2c374380
Fernando Miguel <github@FernandoMiguel.net>
2022-08-23 14:00:45 +0100

    add CRDs
    Signed-off-by: Fernando Miguel <github@FernandoMiguel.net>

--- modules/kubernetes-addons/karpenter/main.tf
+++ modules/kubernetes-addons/karpenter/main.tf
@@ -12,3 +12,25 @@ resource "aws_iam_policy" "karpenter" {
   description = "IAM Policy for Karpenter"
   policy      = data.aws_iam_policy_document.karpenter.json
 }
+
+## karpenter requires manual CRD updates https://karpenter.sh/preview/upgrade-guide/#custom-resource-definition-crd-upgrades
+
+data "http" "karpenter_crd_provisioners" {
+  url = "https://raw.githubusercontent.com/aws/karpenter/v${local.helm_config.version}/charts/karpenter/crds/karpenter.sh_provisioners.yaml"
+}
+
+resource "kubectl_manifest" "karpenter_crd_provisioners" {
+  yaml_body         = data.http.karpenter_crd_provisioners.response_body
+  force_new         = true
+  server_side_apply = true
+}
+
+data "http" "karpenter_crd_awsnodetemplates" {
+  url = "https://raw.githubusercontent.com/aws/karpenter/v${local.helm_config.version}/charts/karpenter/crds/karpenter.k8s.aws_awsnodetemplates.yaml"
+}
+
+resource "kubectl_manifest" "karpenter_crd_awsnodetemplates" {
+  yaml_body         = data.http.karpenter_crd_awsnodetemplates.response_body
+  force_new         = true
+  server_side_apply = true
+}
