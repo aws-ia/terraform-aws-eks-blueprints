8322d6412bbdbc84e2a75d31b2df2346e63991de
Raja Ganesan <rgansan@amazon.com>
2022-09-01 18:25:37 +0800

    Fixes typo in helm config attribute Co-authored-by:  Maria Ana Circulado  <anacirc@amazon.ph>

--- modules/kubernetes-addons/kyverno/main.tf
+++ modules/kubernetes-addons/kyverno/main.tf
@@ -19,11 +19,11 @@ module "kyverno_helm_addon" {
 }
 
 module "kyverno_policies_helm_addon" {
-  source               = "../helm-addon"
-  manage_via_gitops    = var.manage_via_gitops
-  policies_helm_config = local.kyverno_policies_helm_config
-  irsa_config          = null
-  addon_context        = var.addon_context
+  source            = "../helm-addon"
+  manage_via_gitops = var.manage_via_gitops
+  helm_config       = local.kyverno_policies_helm_config
+  irsa_config       = null
+  addon_context     = var.addon_context
 
   depends_on = [kubernetes_namespace_v1.this, module.kyverno_helm_addon]
 }
