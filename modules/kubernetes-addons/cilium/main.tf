f2541cc8e90a1e1a0486a13fd4919fd7da355519
florentio <desouza.florentio@gmail.com>
2022-09-06 18:30:42 -0400

    fix: wrong parameter

--- modules/kubernetes-addons/cilium/main.tf
+++ modules/kubernetes-addons/cilium/main.tf
@@ -3,5 +3,4 @@ module "helm_addon" {
   helm_config       = local.helm_config
   manage_via_gitops = var.manage_via_gitops
   addon_context     = var.addon_context
-  set_values        = local.set_values
 }
