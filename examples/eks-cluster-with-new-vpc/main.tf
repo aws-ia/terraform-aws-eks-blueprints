4f045489f6ac336190de34689e39d679503de146
Rob Grant <rob.grant@nanoporetech.com>
2022-09-13 09:59:35 +0100

    In the Getting Started example, break out hard-coded region into a variable with a default
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- examples/eks-cluster-with-new-vpc/main.tf
+++ examples/eks-cluster-with-new-vpc/main.tf
@@ -26,7 +26,7 @@ locals {
   name = basename(path.cwd)
   # var.cluster_name is for Terratest
   cluster_name = coalesce(var.cluster_name, local.name)
-  region       = "us-west-2"
+  region       = var.region
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
--- examples/eks-cluster-with-new-vpc/variables.tf
+++ examples/eks-cluster-with-new-vpc/variables.tf
@@ -1,6 +1,13 @@
 # tflint-ignore: terraform_unused_declarations
+variable "region" {
+  description = "Target region to deploy in"
+  type        = string
+  default     = "us-west-2"
+}
+
 variable "cluster_name" {
   description = "Name of cluster - used by Terratest for e2e test automation"
   type        = string
   default     = ""
 }
+
