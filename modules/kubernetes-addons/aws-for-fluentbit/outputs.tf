c253f088fa62995e0e06e5b7ce5fa97d74659fa8
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-09-19 05:11:10 -0700

    Update conditional output for cw_log_group_arn

--- modules/kubernetes-addons/aws-for-fluentbit/outputs.tf
+++ modules/kubernetes-addons/aws-for-fluentbit/outputs.tf
@@ -5,7 +5,7 @@ output "cw_log_group_name" {
 
 output "cw_log_group_arn" {
   description = "AWS Fluent Bit CloudWatch Log Group ARN"
-  value       = var.create_cw_log_group ? aws_cloudwatch_log_group.aws_for_fluent_bit[0].arn : 0
+  value       = var.create_cw_log_group ? aws_cloudwatch_log_group.aws_for_fluent_bit[0].arn : null
 }
 
 output "argocd_gitops_config" {
