4811f02509aaba947796b7d4ca37f0fde368264a
Bryant Biggs <bryantbiggs@gmail.com>
2022-09-30 16:00:39 -0400

    fix: Remove optional variable attribute experiment from `helm_addon` sub-module

--- modules/kubernetes-addons/helm-addon/variables.tf
+++ modules/kubernetes-addons/helm-addon/variables.tf
@@ -22,37 +22,39 @@ variable "manage_via_gitops" {
 }
 
 variable "irsa_iam_role_name" {
-  type        = string
   description = "IAM role name for IRSA"
+  type        = string
   default     = ""
 }
 
 variable "irsa_config" {
   description = "Input configuration for IRSA module"
-  type = object({
-    kubernetes_namespace              = string
-    create_kubernetes_namespace       = optional(bool)
-    kubernetes_service_account        = string
-    create_kubernetes_service_account = optional(bool)
-    kubernetes_svc_image_pull_secrets = optional(list(string))
-    irsa_iam_policies                 = optional(list(string))
-  })
-  default = null
+  type        = any
+  default     = {}
+  # type = object({
+  #   kubernetes_namespace              = string
+  #   create_kubernetes_namespace       = optional(bool)
+  #   kubernetes_service_account        = string
+  #   create_kubernetes_service_account = optional(bool)
+  #   kubernetes_svc_image_pull_secrets = optional(list(string))
+  #   irsa_iam_policies                 = optional(list(string))
+  # })
 }
 
 variable "addon_context" {
   description = "Input configuration for the addon"
-  type = object({
-    aws_caller_identity_account_id = string
-    aws_caller_identity_arn        = string
-    aws_eks_cluster_endpoint       = string
-    aws_partition_id               = string
-    aws_region_name                = string
-    eks_cluster_id                 = string
-    eks_oidc_issuer_url            = string
-    eks_oidc_provider_arn          = string
-    tags                           = map(string)
-    irsa_iam_role_path             = optional(string)
-    irsa_iam_permissions_boundary  = optional(string)
-  })
+  type        = any
+  # type = object({
+  #   aws_caller_identity_account_id = string
+  #   aws_caller_identity_arn        = string
+  #   aws_eks_cluster_endpoint       = string
+  #   aws_partition_id               = string
+  #   aws_region_name                = string
+  #   eks_cluster_id                 = string
+  #   eks_oidc_issuer_url            = string
+  #   eks_oidc_provider_arn          = string
+  #   tags                           = map(string)
+  #   irsa_iam_role_path             = optional(string)
+  #   irsa_iam_permissions_boundary  = optional(string)
+  # })
 }
--- modules/kubernetes-addons/helm-addon/versions.tf
+++ modules/kubernetes-addons/helm-addon/versions.tf
@@ -1,8 +1,6 @@
 terraform {
   required_version = ">= 1.0.0"
 
-  experiments = [module_variable_optional_attrs]
-
   required_providers {
     helm = {
       source  = "hashicorp/helm"
