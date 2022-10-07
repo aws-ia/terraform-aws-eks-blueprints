8a708f8cce605d76265b2d337a9c4fd4a8af9923
sallaman <sallaman@amazon.com>
2022-10-07 15:51:09 +0000

    remove test on main

--- examples/eks-cluster-with-external-dns/main.tf
+++ examples/eks-cluster-with-external-dns/main.tf
@@ -28,8 +28,8 @@ data "aws_acm_certificate" "issued" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  name   = basename(path.cwd)
-  region = "us-west-2"
+  name   = "dns"
+  region = "eu-west-1"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
