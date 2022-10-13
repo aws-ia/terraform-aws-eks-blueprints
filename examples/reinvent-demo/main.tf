c3abcf591686029858b8e7be56c7abee2eecf773
Zvikan <zvi8875@gmail.com>
2022-10-13 13:58:12 -0500

    fix: region to us-west-2

--- examples/reinvent-demo/main.tf
+++ examples/reinvent-demo/main.tf
@@ -56,7 +56,7 @@ data "aws_eks_addon_version" "latest" {
 ###############################################################################
 locals {
   name   = basename(path.cwd)
-  region = "us-east-1"
+  region = "us-west-2"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
