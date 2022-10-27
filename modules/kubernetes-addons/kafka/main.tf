18a9f3dd3e1e92776c0b6ca9862bbbd2cda98699
Aly Ibrahim <aly.ibrahim@gmail.com>
2022-10-27 09:25:58 +0000

    Accomodate for PR #1011 [fix: Remove optional variable attribute experiment from project]

--- modules/kubernetes-addons/kafka/main.tf
+++ modules/kubernetes-addons/kafka/main.tf
@@ -21,6 +21,6 @@ locals {
 module "helm_addon" {
   source        = "../helm-addon"
   helm_config   = local.helm_config
-  irsa_config   = null
+#  irsa_config   = {}
   addon_context = var.addon_context
 }
