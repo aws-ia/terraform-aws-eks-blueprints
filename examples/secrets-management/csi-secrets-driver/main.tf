19299525fb83267811a79891aa221a270f5ecfd7
Bryant Biggs <bryantbiggs@gmail.com>
2022-10-31 17:10:39 -0400

    fix: Correct late addition merged in

--- examples/secrets-management/csi-secrets-driver/main.tf
+++ examples/secrets-management/csi-secrets-driver/main.tf
@@ -18,8 +18,8 @@ provider "helm" {
 
 provider "kubectl" {
   apply_retry_count      = 10
-  host                   = module.eks_blueprints.eks_cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   load_config_file       = false
   token                  = data.aws_eks_cluster_auth.this.token
 }
