8d549255224abc5f7c66a10015a58ad304375dca
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-11-04 11:08:50 -0700

    remove cluster_name var

--- examples/wireguard-with-cilium/variables.tf
+++ examples/wireguard-with-cilium/variables.tf
@@ -1,10 +1,4 @@
 # tflint-ignore: terraform_unused_declarations
-variable "cluster_name" {
-  description = "Name of cluster - used by Terratest for e2e test automation"
-  type        = string
-  default     = ""
-}
-
 variable "enable_example" {
   description = "Enable example to test this blueprint"
   type        = bool
