f468bb1edc14b26b94368dd83b453263e8d34216
tgeo <kavadias@gmail.com>
2022-11-09 18:27:08 +0200

    fix annoying rancher annotations

--- modules/irsa/main.tf
+++ modules/irsa/main.tf
@@ -7,7 +7,12 @@ resource "kubernetes_namespace_v1" "irsa" {
   metadata {
     name = var.kubernetes_namespace
   }
-
+  lifecycle {
+    ignore_changes = [
+      metadata[0].annotations["cattle.io/status"],
+      metadata[0].annotations["lifecycle.cattle.io/create.namespace-auth"]
+    ]
+  }
   timeouts {
     delete = "15m"
   }
