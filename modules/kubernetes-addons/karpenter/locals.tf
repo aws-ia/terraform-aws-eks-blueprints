03a55fae11275404c69a097ba38c8b81a401099a
Fernando Miguel <github@FernandoMiguel.net>
2022-11-17 19:34:37 +0000

    local oops
    Signed-off-by: Fernando Miguel <github@FernandoMiguel.net>

--- modules/kubernetes-addons/karpenter/locals.tf
+++ modules/kubernetes-addons/karpenter/locals.tf
@@ -10,7 +10,7 @@ locals {
       value = false
     }
   ]
-  karpenter_sqs_queue_arn = "arn:aws:sqs:${var.addon_context["aws_region_name"]}:${var.addon_context["aws_caller_identity_account_id"]}:${var.addon_context["eks_cluster_id"]}"
+  karpenter_sqs_queue_arn = var.karpenter_sqs_queue_arn != null ? var.karpenter_sqs_queue_arn : "arn:aws:sqs:${var.addon_context["aws_region_name"]}:${var.addon_context["aws_caller_identity_account_id"]}:${var.addon_context["eks_cluster_id"]}"
 
   # https://github.com/aws/karpenter/blob/main/charts/karpenter/Chart.yaml
   helm_config = merge(
