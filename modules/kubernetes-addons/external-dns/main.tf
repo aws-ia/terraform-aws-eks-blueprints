a409e0a47d6cd40d6eaf080df643127cf854aef4
Sébastien Allamand <sallaman@amazon.com>
2022-11-19 02:44:14 +0100

    feat: External dns argo values (#1035)
    Co-authored-by: Apoorva Kulkarni <kuapoorv@amazon.com>
    Co-authored-by: Apoorva Kulkarni <askulkarni84@gmail.com>
    Co-authored-by: Bryant Biggs <bryantbiggs@gmail.com>

--- modules/kubernetes-addons/external-dns/main.tf
+++ modules/kubernetes-addons/external-dns/main.tf
@@ -2,10 +2,13 @@ locals {
   name            = try(var.helm_config.name, "external-dns")
   service_account = try(var.helm_config.service_account, "${local.name}-sa")
 
-  argocd_gitops_config = {
-    enable             = true
-    serviceAccountName = local.service_account
-  }
+  argocd_gitops_config = merge(
+    {
+      enable             = true
+      serviceAccountName = local.service_account
+    },
+    var.helm_config
+  )
 }
 
 module "helm_addon" {
