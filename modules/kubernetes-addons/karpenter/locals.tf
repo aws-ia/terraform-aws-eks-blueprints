ded768a39f8105b94fe9779b1f10744442ef70d4
Fernando Miguel <168366+FernandoMiguel@users.noreply.github.com>
2022-11-21 11:04:05 +0000

    karpenter v0.19.1

--- modules/kubernetes-addons/karpenter/locals.tf
+++ modules/kubernetes-addons/karpenter/locals.tf
@@ -18,7 +18,7 @@ locals {
       name       = local.name
       chart      = local.name
       repository = "oci://public.ecr.aws/karpenter"
-      version    = "v0.19.0"
+      version    = "v0.19.1"
       namespace  = local.name
       values = [
         <<-EOT
