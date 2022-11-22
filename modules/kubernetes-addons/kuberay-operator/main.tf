077d927e235300da9064773b0ca5089e55731aab
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-11-22 00:18:35 -0800

    update chart name

--- modules/kubernetes-addons/kuberay-operator/main.tf
+++ modules/kubernetes-addons/kuberay-operator/main.tf
@@ -15,8 +15,8 @@ module "helm_addon" {
   # https://github.com/ray-project/kuberay/blob/master/helm-chart/kuberay-operator/Chart.yaml
   helm_config = merge(
     {
-      name        = local.name
-      chart       = "https://ray-project.github.io/kuberay-helm"
+      name        = "kuberay/${local.name}"
+      chart       = "https://ray-project.github.io/kuberay-helm/"
       version     = "0.4.0"
       namespace   = kubernetes_namespace_v1.this.metadata[0].name
       description = "KubeRay Operator Helm Chart deployment configuration"
