136d955f6f0d9f4238519552d7744b567a19a465
Your Name <you@example.com>
2022-11-24 22:44:27 +0400

    Some changes for pre-commit workflow

--- examples/gitops/argocd-apps/main.tf
+++ examples/gitops/argocd-apps/main.tf
@@ -21,7 +21,7 @@ data "aws_eks_cluster_auth" "this" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  name   = basename(path.cwd)
+  name = basename(path.cwd)
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
