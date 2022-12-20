8a1aeaf8482225568da934b2682e357aae2966bb
Juan Aguilar Santillana <mhpoin@gmail.com>
2022-12-20 20:02:46 +0100

    fix: argocd addons error, bool expressions must have consistent types

--- modules/kubernetes-addons/argocd/main.tf
+++ modules/kubernetes-addons/argocd/main.tf
@@ -69,7 +69,7 @@ resource "helm_release" "argocd_application" {
       { repoUrl = each.value.repo_url },
       each.value.values,
       local.global_application_values,
-      each.value.add_on_application ? var.addon_config : {}
+      each.value.add_on_application ? var.addon_config : null
     ))
     type = "auto"
   }
