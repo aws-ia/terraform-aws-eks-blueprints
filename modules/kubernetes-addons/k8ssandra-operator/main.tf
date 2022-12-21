0eaa4c9b8ea0ac786afd0f6290ead7df30fca06f
Aly Ibrahim <aly.ibrahim@gmail.com>
2022-12-21 21:14:47 +0000

    K8ssandra Operator for Single Cluster

---
+++ modules/kubernetes-addons/k8ssandra-operator/main.tf
@@ -0,0 +1,24 @@
+locals {
+  name = "k8ssandra-operator"
+  default_helm_config = {
+    name             = local.name
+    chart            = "k8ssandra-operator"
+    repository       = "https://helm.k8ssandra.io/stable"
+    version          = "0.39.1"
+    namespace        = local.name
+    create_namespace = true
+    values           = [templatefile("${path.module}/values.yaml", {})]
+    description      = "K8ssandra Operator to run Cassandra DB on Kubernetes"
+  }
+  helm_config = merge(local.default_helm_config, var.helm_config)
+}
+
+#-------------------------------------------------
+# K8assandra Operator Helm Add-on
+#-------------------------------------------------
+module "helm_addon" {
+  source            = "../helm-addon"
+  helm_config       = local.helm_config
+  addon_context     = var.addon_context
+  manage_via_gitops = var.manage_via_gitops
+}
