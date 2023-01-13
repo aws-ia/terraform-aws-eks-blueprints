02a3c89906d58e6d4fea962c334a0e70e6dc822f
Bryant Biggs <bryantbiggs@gmail.com>
2023-01-13 09:49:01 -0500

    fix: Update to use EKS Fargate link and account for the 256Mb overhead

--- examples/analytics/emr-on-eks-fargate/main.tf
+++ examples/analytics/emr-on-eks-fargate/main.tf
@@ -54,15 +54,23 @@ module "eks" {
       configuration_values = jsonencode({
         computeType = "Fargate"
         # Ensure that the we fully utilize the minimum amount of resources that are supplied by
-        # Fargate https://docs.aws.amazon.com/AmazonECS/latest/developerguide/AWS_Fargate.html
+        # Fargate https://docs.aws.amazon.com/eks/latest/userguide/fargate-pod-configuration.html
+        # Fargate adds 256 MB to each pod's memory reservation for the required Kubernetes
+        # components (kubelet, kube-proxy, and containerd). Fargate rounds up to the following
+        # compute configuration that most closely matches the sum of vCPU and memory requests in
+        # order to ensure pods always have the resources that they need to run.
         resources = {
           limits = {
-            cpu    = "0.25"
-            memory = "250Mi" # Going above this value will allocate a task with 1Gb of memory
+            cpu = "0.25"
+            # We are targetting the smallest Task size of 512Mb, so we subtract 256Mb from the
+            # request/limit to ensure we can fit within that task
+            memory = "256M"
           }
           requests = {
-            cpu    = "0.25"
-            memory = "250Mi" # Going above this value will allocate a task with 1Gb of memory
+            cpu = "0.25"
+            # We are targetting the smallest Task size of 512Mb, so we subtract 256Mb from the
+            # request/limit to ensure we can fit within that task
+            memory = "256M"
           }
         }
       })
