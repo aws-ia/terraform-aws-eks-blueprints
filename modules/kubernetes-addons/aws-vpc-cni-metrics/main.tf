4ebd5e0090049fc173df45deb96f5acb89aab1ef
Carlos Santana <csantana23@gmail.com>
2023-01-19 23:08:21 -0500

    one main.tf
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- modules/kubernetes-addons/aws-vpc-cni-metrics/main.tf
+++ modules/kubernetes-addons/aws-vpc-cni-metrics/main.tf
@@ -1,3 +1,48 @@
+locals {
+  name            = "cni-metrics-helper"
+  service_account = try(var.helm_config.service_account, local.name)
+  version         = var.addon_version
+
+  default_helm_config = {
+    name             = local.name
+    chart            = local.name
+    repository       = "https://aws.github.io/eks-charts"
+    version          = "0.1.15"
+    namespace        = "kube-system"
+    create_namespace = false
+    values           = null
+    description      = "aws-load-balancer-controller Helm Chart for ingress resources"
+  }
+
+  helm_config = merge(
+    local.default_helm_config,
+    var.helm_config
+  )
+
+  set_values = concat(
+    [
+      {
+        name  = "serviceAccount.name"
+        value = local.service_account
+      },
+      {
+        name  = "serviceAccount.create"
+        value = true
+      },
+      {
+        name  = "env.AWS_VPC_K8S_CNI_LOGLEVEL"
+        value = "INFO"
+      },
+      {
+        name  = "image.override"
+        value = "${var.addon_context.default_repository}/cni-metrics-helper:${local.version}"
+      }
+    ],
+    try(var.helm_config.set_values, [])
+  )
+
+}
+
 module "vpc_cni_metrics_addon" {
   source = "github.com/aws-ia/terraform-aws-eks-addon?ref=v1.0.0"
 
@@ -42,4 +87,15 @@ resource "aws_iam_policy" "aws_vpc_cni_metrics" {
   policy      = data.aws_iam_policy_document.aws_vpc_cni_metrics.json
 
   tags = var.addon_context.tags
+}
+
+data "aws_iam_policy_document" "aws_vpc_cni_metrics" {
+  statement {
+    sid = "CNIMetrics"
+    actions = [
+      "cloudwatch:PutMetricData",
+      "ec2:DescribeTags"
+    ]
+    resources = ["*"]
+  }
 }
