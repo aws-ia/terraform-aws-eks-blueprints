098d669cee45c26ec68f9c63f1fc64950e354cc9
Rodrigo Bersa <bersr@amazon.com>
2023-01-24 21:09:03 -0500

    pre-commit: fix fmt, end of files, and docs on `modules/kubernetes-addons/external-secrets`

--- modules/kubernetes-addons/external-secrets/data.tf
+++ modules/kubernetes-addons/external-secrets/data.tf
@@ -1,8 +1,8 @@
 data "aws_iam_policy_document" "external_secrets" {
   statement {
-    actions   = ["ssm:GetParameter"]
+    actions = ["ssm:GetParameter"]
     resources = concat(
-      var.external_secrets_ssm_parameter_arns, 
+      var.external_secrets_ssm_parameter_arns,
       ["arn:${var.addon_context.aws_partition_id}:ssm:${var.addon_context.aws_region_name}:${var.addon_context.aws_caller_identity_account_id}:parameter/*"]
     )
   }
