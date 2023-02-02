629107de41e9ee85abb27b0a2f97023a54fe56d9
Austin Heiman <atheimanksu@gmail.com>
2023-02-02 09:50:41 -0600

    create ns if not kube-system

--- modules/kubernetes-addons/csi-secrets-store-provider-aws/main.tf
+++ modules/kubernetes-addons/csi-secrets-store-provider-aws/main.tf
@@ -1,10 +1,17 @@
 locals {
   name      = try(var.helm_config.name, "csi-secrets-store-provider-aws")
-  namespace = try(var.helm_config.namespace, "secrets-store-csi-driver")
+  namespace = try(var.helm_config.namespace, "kube-system")
+}
+
+resource "kubernetes_namespace_v1" "csi_secrets_store_provider_aws" {
+  count = try(var.helm_config["create_namespace"], true) && local.namespace != "kube-system" ? 1 : 0
+  metadata {
+    name = local.namespace
+  }
 }
 
 module "helm_addon" {
-  source = "../helm-addon"
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints/modules/kubernetes-addons/helm-addon"
 
   # https://github.com/aws/eks-charts/blob/master/stable/csi-secrets-store-provider-aws/Chart.yaml
   helm_config = merge(
