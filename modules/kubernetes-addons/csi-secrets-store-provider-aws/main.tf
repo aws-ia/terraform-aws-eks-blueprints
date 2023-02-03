5eae3815010e8ff8d82198ad004b529caacaa044
Austin Heiman <atheimanksu@gmail.com>
2023-02-03 06:42:10 -0600

    Revert to relative path to helm addon

--- modules/kubernetes-addons/csi-secrets-store-provider-aws/main.tf
+++ modules/kubernetes-addons/csi-secrets-store-provider-aws/main.tf
@@ -11,7 +11,7 @@ resource "kubernetes_namespace_v1" "csi_secrets_store_provider_aws" {
 }
 
 module "helm_addon" {
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints/modules/kubernetes-addons/helm-addon"
+  source = "../helm-addon"
 
   # https://github.com/aws/secrets-store-csi-driver-provider-aws/blob/main/charts/secrets-store-csi-driver-provider-aws/Chart.yaml
   helm_config = merge(
