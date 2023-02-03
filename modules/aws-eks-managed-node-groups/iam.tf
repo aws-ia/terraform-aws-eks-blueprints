c6f45735d079f27a94d0ac0e770d768457e9ebaa
Austin Heiman <atheimanksu@gmail.com>
2023-02-03 14:33:37 -0600

    fix: Write assume_role_policy without data source

--- modules/aws-eks-managed-node-groups/iam.tf
+++ modules/aws-eks-managed-node-groups/iam.tf
@@ -1,7 +1,19 @@
 resource "aws_iam_role" "managed_ng" {
   name                  = "${var.context.eks_cluster_id}-${local.managed_node_group["node_group_name"]}"
   description           = "EKS Managed Node group IAM Role"
-  assume_role_policy    = data.aws_iam_policy_document.managed_ng_assume_role_policy.json
+  assume_role_policy    = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = "EKSWorkerAssumeRole"
+        Principal = {
+          Service = [local.ec2_principal]
+        }
+      }
+    ]
+  })
   path                  = var.context.iam_role_path
   permissions_boundary  = var.context.iam_role_permissions_boundary
   force_detach_policies = true
