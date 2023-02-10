36fb6882e9937e0d4508d48d60955bcf07142db1
Carlos Santana <csantana23@gmail.com>
2023-02-09 21:50:05 -0500

    update destroy.sh
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- examples/gitops/argocd-multi-cluster/hub-cluster/destroy.sh
+++ examples/gitops/argocd-multi-cluster/hub-cluster/destroy.sh
@@ -3,6 +3,6 @@
 set -xe
 
 terraform destroy -target="module.eks_blueprints_kubernetes_addons" -auto-approve
-terraform destroy -target="module.eks_blueprints" -auto-approve
+terraform destroy -target="module.eks" -auto-approve
 terraform destroy -target="module.vpc" -auto-approve
 terraform destroy -auto-approve
--- examples/gitops/argocd-multi-cluster/spoke-cluster-1/destroy.sh
+++ examples/gitops/argocd-multi-cluster/spoke-cluster-1/destroy.sh
@@ -4,6 +4,6 @@ set -xe
 
 terraform destroy -target="module.spoke_cluster.module.eks_blueprints_argocd_addon" -auto-approve
 terraform destroy -target="module.spoke_cluster.module.eks_blueprints_kubernetes_addons" -auto-approve
-terraform destroy -target="module.spoke_cluster.module.eks_blueprints" -auto-approve
+terraform destroy -target="module.spoke_cluster.module.eks" -auto-approve
 terraform destroy -target="module.spoke_cluster.module.vpc" -auto-approve
 terraform destroy -auto-approve
