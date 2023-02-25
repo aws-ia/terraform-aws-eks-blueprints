a9181ae2ddfafc03af70b1eab667eac7dd17bf1a
Vara Bonthu <vara.bonthu@gmail.com>
2023-02-25 20:50:45 +0000

    feat: Added SQS queue creation with events for Karpenter  (#1458)

--- modules/kubernetes-addons/karpenter/outputs.tf
+++ modules/kubernetes-addons/karpenter/outputs.tf
@@ -22,3 +22,23 @@ output "service_account" {
   description = "Name of Kubernetes service account"
   value       = module.helm_addon.service_account
 }
+
+output "sqs_queue_arn" {
+  description = "The ARN of the SQS queue"
+  value       = try(aws_sqs_queue.this[0].arn, null)
+}
+
+output "sqs_queue_name" {
+  description = "The name of the created Amazon SQS queue"
+  value       = try(aws_sqs_queue.this[0].name, null)
+}
+
+output "sqs_queue_url" {
+  description = "The URL for the created Amazon SQS queue"
+  value       = try(aws_sqs_queue.this[0].url, null)
+}
+
+output "event_rules" {
+  description = "Map of the event rules created and their attributes"
+  value       = aws_cloudwatch_event_rule.this
+}
