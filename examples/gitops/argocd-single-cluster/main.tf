a5c3f00466f1b3e26d4739ebb777c3ace04ac101
Carlos Santana <csantana23@gmail.com>
2023-03-03 22:12:23 -0500

    fix path on single cluster
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- examples/gitops/argocd-single-cluster/main.tf
+++ examples/gitops/argocd-single-cluster/main.tf
@@ -80,7 +80,7 @@ module "eks" {
 ################################################################################
 
 module "eks_blueprints_kubernetes_addons" {
-  source = "../../modules/kubernetes-addons"
+  source = "../../../modules/kubernetes-addons"
 
   eks_cluster_id       = module.eks.cluster_name
   eks_cluster_endpoint = module.eks.cluster_endpoint
