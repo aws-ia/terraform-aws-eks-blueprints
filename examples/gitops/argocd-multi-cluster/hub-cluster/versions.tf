e4d2f07022a88741ba102bf186697f88be7afd03
Carlos Santana <csantana23@gmail.com>
2023-03-11 21:14:01 -0500

    remove password
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- examples/gitops/argocd-multi-cluster/hub-cluster/versions.tf
+++ examples/gitops/argocd-multi-cluster/hub-cluster/versions.tf
@@ -18,13 +18,5 @@ terraform {
       source  = "gavinbunney/kubectl"
       version = ">= 1.14"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.3.2"
-    }
-    bcrypt = {
-      source  = "viktorradnai/bcrypt"
-      version = ">= 0.1.2"
-    }
   }
 }
