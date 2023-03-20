4f4d4c9f26c0744d0028da1e9dd873df7b9d46fc
Daniel Ciaglia <daniel@sigterm.de>
2023-03-20 18:59:26 +0100

    change order to be less disruptive on update of policy

--- modules/kubernetes-addons/external-secrets/data.tf
+++ modules/kubernetes-addons/external-secrets/data.tf
@@ -6,13 +6,6 @@ data "aws_iam_policy_document" "external_secrets" {
       ["arn:${var.addon_context.aws_partition_id}:ssm:${var.addon_context.aws_region_name}:${var.addon_context.aws_caller_identity_account_id}:parameter/*"]
     )
   }
-  statement {
-    # it seems `ssm:DescribeParameters` needs wildcard on resources.
-    actions = ["ssm:DescribeParameters"]
-    resources = concat(
-      ["arn:${var.addon_context.aws_partition_id}:ssm:${var.addon_context.aws_region_name}:${var.addon_context.aws_caller_identity_account_id}:*"]
-    )
-  }
 
   statement {
     actions = [
@@ -24,6 +17,14 @@ data "aws_iam_policy_document" "external_secrets" {
     resources = concat(
       var.external_secrets_secrets_manager_arns,
       ["arn:${var.addon_context.aws_partition_id}:secretsmanager:${var.addon_context.aws_region_name}:${var.addon_context.aws_caller_identity_account_id}:secret:*"]
+    )
+  }
+
+  statement {
+    # it seems `ssm:DescribeParameters` needs wildcard on resources.
+    actions = ["ssm:DescribeParameters"]
+    resources = concat(
+      ["arn:${var.addon_context.aws_partition_id}:ssm:${var.addon_context.aws_region_name}:${var.addon_context.aws_caller_identity_account_id}:*"]
     )
   }
 }
