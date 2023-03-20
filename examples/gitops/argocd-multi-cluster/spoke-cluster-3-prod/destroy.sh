63dc70a1b75b29d450a448f7673c66d9fec0a67c
Carlos Santana <csantana23@gmail.com>
2023-03-20 18:21:26 -0400

    no need to sleep, the argocd app with helm has a timeout of 20mins now
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- examples/gitops/argocd-multi-cluster/spoke-cluster-3-prod/destroy.sh
+++ examples/gitops/argocd-multi-cluster/spoke-cluster-3-prod/destroy.sh
@@ -3,9 +3,7 @@
 set -xe
 
 terraform destroy -target="module.spoke_cluster.module.eks_blueprints_argocd_workloads" -auto-approve
-sleep 60 # wait for argocd apps to be deleted
 terraform destroy -target="module.spoke_cluster.module.eks_blueprints_argocd_addons" -auto-approve
-sleep 60 # wait for argocd apps to be deleted
 terraform destroy -target="module.spoke_cluster.module.eks_blueprints_kubernetes_addons" -auto-approve
 terraform destroy -target="module.spoke_cluster.module.eks" -auto-approve
 terraform destroy -target="module.spoke_cluster.module.vpc" -auto-approve
--- examples/gitops/argocd-multi-cluster/spoke-cluster-template/destroy.sh
+++ examples/gitops/argocd-multi-cluster/spoke-cluster-template/destroy.sh
@@ -3,9 +3,7 @@
 set -xe
 
 terraform destroy -target="module.eks_blueprints_argocd_workloads" -auto-approve
-sleep 60 # wait for argocd apps to be deleted
 terraform destroy -target="module.eks_blueprints_argocd_addons" -auto-approve
-sleep 60 # wait for argocd apps to be deleted
 terraform destroy -target="module.eks_blueprints_kubernetes_addons" -auto-approve
 terraform destroy -target="module.eks" -auto-approve
 terraform destroy -target="module.vpc" -auto-approve
