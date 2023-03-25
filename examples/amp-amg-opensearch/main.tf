edb93b1d186c00ec87b59418ca8ea7f5f15f5c3e
Carlos Santana <csantana23@gmail.com>
2023-03-24 22:52:16 -0400

    restore amg example
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- examples/amp-amg-opensearch/main.tf
+++ examples/amp-amg-opensearch/main.tf
@@ -128,12 +128,12 @@ resource "grafana_data_source" "prometheus" {
   is_default = true
   url        = module.managed_prometheus.workspace_prometheus_endpoint
 
-  json_data_encoded = jsonencode({
+  json_data {
     http_method     = "POST"
     sigv4_auth      = true
     sigv4_auth_type = "workspace-iam-role"
     sigv4_region    = local.region
-  })
+  }
 }
 
 #---------------------------------------------------------------
