876dad30e144503eb75ff52b19dfbbf345cc4d09
Andrei Dumitra <andrei.dumitra@pennylane.tech>
2023-03-31 09:01:22 +0300

    allow more restrictive policies

--- modules/kubernetes-addons/external-secrets/data.tf
+++ modules/kubernetes-addons/external-secrets/data.tf
@@ -1,10 +1,7 @@
 data "aws_iam_policy_document" "external_secrets" {
   statement {
-    actions = ["ssm:GetParameter"]
-    resources = concat(
-      var.external_secrets_ssm_parameter_arns,
-      ["arn:${var.addon_context.aws_partition_id}:ssm:${var.addon_context.aws_region_name}:${var.addon_context.aws_caller_identity_account_id}:parameter/*"]
-    )
+    actions   = ["ssm:GetParameter"]
+    resources = length(var.external_secrets_ssm_parameter_arns) > 0 ? var.external_secrets_ssm_parameter_arns : ["arn:${var.addon_context.aws_partition_id}:ssm:${var.addon_context.aws_region_name}:${var.addon_context.aws_caller_identity_account_id}:parameter/*"]
   }
 
   statement {
@@ -14,9 +11,6 @@ data "aws_iam_policy_document" "external_secrets" {
       "secretsmanager:DescribeSecret",
       "secretsmanager:ListSecretVersionIds",
     ]
-    resources = concat(
-      var.external_secrets_secrets_manager_arns,
-      ["arn:${var.addon_context.aws_partition_id}:secretsmanager:${var.addon_context.aws_region_name}:${var.addon_context.aws_caller_identity_account_id}:secret:*"]
-    )
+    resources = length(var.external_secrets_secrets_manager_arns) > 0 ? var.external_secrets_secrets_manager_arns : ["arn:${var.addon_context.aws_partition_id}:secretsmanager:${var.addon_context.aws_region_name}:${var.addon_context.aws_caller_identity_account_id}:secret:*"]
   }
 }
