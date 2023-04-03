be649761a4e8002beb16a0b9482f97e9727e6cc7
mshaul <mshaul@netapp.com>
2023-04-03 16:14:09 +0300

    remove argocd output from fsxn module

--- modules/kubernetes-addons/aws-fsxn-csi-driver/outputs.tf
+++ modules/kubernetes-addons/aws-fsxn-csi-driver/outputs.tf
@@ -1,8 +1,3 @@
-output "argocd_gitops_config" {
-  description = "Configuration used for managing the add-on with ArgoCD"
-  value       = var.manage_via_gitops ? local.argocd_gitops_config : null
-}
-
 output "release_metadata" {
   description = "Map of attributes of the Helm release metadata"
   value       = module.helm_addon.release_metadata
