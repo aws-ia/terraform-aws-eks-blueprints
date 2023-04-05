5efa16bca83f54c4c593e79c7f8cd56fcbdd55a7
Apoorva Kulkarni <kuapoorv@amazon.com>
2023-04-05 16:18:47 -0700

    fix: csi secrets provider namespace issue due to deprecation

--- modules/kubernetes-addons/csi-secrets-store-provider-aws/main.tf
+++ modules/kubernetes-addons/csi-secrets-store-provider-aws/main.tf
@@ -1,9 +1,17 @@
 locals {
-  name      = try(var.helm_config.name, "csi-secrets-store-provider-aws")
+  name      = try(var.helm_config.name, "secrets-store-csi-driver-provider-aws")
   namespace = try(var.helm_config.namespace, "kube-system")
 }
 
+module "secrets_store_csi_driver" {
+  source = "../secrets-store-csi-driver"
+
+  addon_context = var.addon_context
+}
+
 resource "kubernetes_namespace_v1" "csi_secrets_store_provider_aws" {
+  count = local.namespace == "kube-system" ? 0 : 1
+
   metadata {
     name = local.namespace
   }
@@ -12,19 +20,24 @@ resource "kubernetes_namespace_v1" "csi_secrets_store_provider_aws" {
 module "helm_addon" {
   source = "../helm-addon"
 
-  # https://github.com/aws/eks-charts/blob/master/stable/csi-secrets-store-provider-aws/Chart.yaml
+  # https://github.com/aws/secrets-store-csi-driver-provider-aws/blob/main/charts/secrets-store-csi-driver-provider-aws/Chart.yaml
   helm_config = merge(
     {
       name        = local.name
       chart       = local.name
-      repository  = "https://aws.github.io/eks-charts"
-      version     = "0.0.3"
-      namespace   = kubernetes_namespace_v1.csi_secrets_store_provider_aws.metadata[0].name
-      description = "A Helm chart to install the Secrets Store CSI Driver and the AWS Key Management Service Provider inside a Kubernetes cluster."
+      repository  = "https://aws.github.io/secrets-store-csi-driver-provider-aws"
+      version     = "0.3.2"
+      namespace   = local.namespace
+      description = "A Helm chart for the AWS Secrets Manager and Config Provider for Secret Store CSI Driver."
     },
     var.helm_config
   )
 
   manage_via_gitops = var.manage_via_gitops
   addon_context     = var.addon_context
+
+  depends_on = [
+    kubernetes_namespace_v1.csi_secrets_store_provider_aws,
+    module.secrets_store_csi_driver
+  ]
 }
