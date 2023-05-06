7fb578a5265658ad14d1aa5f316baec2ba18d298
jrsalgado <jayrosalgado@gmail.com>
2023-05-06 09:05:54 -0700

    remove duplicate data

--- data.tf
+++ data.tf
@@ -1,9 +1,6 @@
 data "aws_partition" "current" {}
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
-data "aws_iam_session_context" "current" {
-  arn = data.aws_caller_identity.current.arn
-}
 
 data "aws_eks_cluster" "cluster" {
   count = var.create_eks ? 1 : 0
