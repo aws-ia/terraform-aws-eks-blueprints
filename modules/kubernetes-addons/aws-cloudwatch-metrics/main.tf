f6d7063b8ae5728a81c6606d81247b26a4043068
Bryant Biggs <bryantbiggs@gmail.com>
2023-06-03 08:54:54 -0400

    fix: Remove Terraform module code from project (#1628)

--- modules/kubernetes-addons/aws-cloudwatch-metrics/main.tf
+++
@@ -1,8 +0,0 @@
-module "helm_addon" {
-  source            = "../helm-addon"
-  manage_via_gitops = var.manage_via_gitops
-  set_values        = local.set_values
-  helm_config       = local.helm_config
-  irsa_config       = local.irsa_config
-  addon_context     = var.addon_context
-}
