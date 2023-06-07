5f3a1c95d05d3ee093d5c31fd280337aebfda45f
Bryant Biggs <bryantbiggs@gmail.com>
2023-06-06 20:02:38 -0400

    fix: Correct CI ignore due to directory name change

--- .github/scripts/plan-examples.py
+++ .github/scripts/plan-examples.py
@@ -10,7 +10,7 @@ def get_examples():
     """
     exclude = {
         'examples/appmesh-mtls',  # excluded until Rout53 is setup
-        'examples/blue-green-upgrade/core-infra',
+        'examples/blue-green-upgrade/environment',
         'examples/blue-green-upgrade/modules/eks_cluster'
     }
 
