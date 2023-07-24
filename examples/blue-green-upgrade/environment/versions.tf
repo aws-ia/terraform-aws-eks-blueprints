26c8624831b07181cac2e847f3ae024fc3c6d799
yukkes <44968006+yukkes@users.noreply.github.com>
2023-07-24 21:06:01 +0900

    fix: Modify Fargate serverless example to wait for Fargate profiles to provision (#1696)
    Co-authored-by: Bryant Biggs <bryantbiggs@gmail.com>

--- examples/blue-green-upgrade/environment/versions.tf
+++ examples/blue-green-upgrade/environment/versions.tf
@@ -1,13 +1,14 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.72.0"
+      version = ">= 4.67"
     }
     random = {
-      version = ">= 3"
+      source  = "hashicorp/random"
+      version = ">= 3.0"
     }
   }
 }
