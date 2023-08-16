855d12049620266e4cc9e3cd81bd27fd209058ef
Rob <robertnorthard@googlemail.com>
2023-08-16 15:31:44 +0100

    Added eks private public ingress example for nginx

---
+++ docs/blueprints/private-public-ingress.md
@@ -0,0 +1,7 @@
+---
+title: Private and Public Nginx Ingress
+---
+
+{%
+   include-markdown "../../examples/private-public-ingress/README.md"
+%}
---
+++ examples/private-public-ingress/providers.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "me-central-1"
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "virginia"
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+provider "kubectl" {
+  apply_retry_count      = 10
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+  token                  = data.aws_eks_cluster_auth.this.token
+}
