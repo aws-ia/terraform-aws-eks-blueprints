2d7ea7eef8dbab515b1c26ca4dda72f4a12ca6c5
Robert Northard <northard@amazon.com>
2023-08-16 19:44:12 +0100

    Add code comment

--- examples/private-public-ingress/main.tf
+++ examples/private-public-ingress/main.tf
@@ -26,14 +26,14 @@ resource "aws_security_group" "nginx-ingress-external-sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] // modify to your requirements
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] // modify to your requirements
   }
 
   egress {
