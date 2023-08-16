03db0c83f322a1ce48b229a7b79f95c15eaaf7b2
Tsahi Duek <tsahiduek@gmail.com>
2023-08-16 20:58:14 +0100

    feat: Istio multi primary single network blueprint (#1727)
    Co-authored-by: Apoorva Kulkarni <kuapoorv@amazon.com>
    Co-authored-by: Rodrigo Bersa <rodrigo.bersa@gmail.com>

---
+++ examples/istio-multi-cluster/1.cluster1/variables.tf
@@ -0,0 +1,12 @@
+
+variable "name" {
+  description = "cluster name"
+  type        = string
+  default     = "cluster-1"
+}
+
+variable "region" {
+  description = "cluster name"
+  type        = string
+  default     = "eu-west-1"
+}
