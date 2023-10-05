d6d9af623363778d7c585d93e13b5f8a6547e0e3
Ofer Kafry <125174789+oferca@users.noreply.github.com>
2023-10-05 18:30:42 +0300

    Update data.tf

--- modules/aws-eks-managed-node-groups/data.tf
+++ modules/aws-eks-managed-node-groups/data.tf
@@ -1,14 +1,15 @@
-data "aws_iam_policy_document" "managed_ng_assume_role_policy" {
-  statement {
-    sid = "EKSWorkerAssumeRole"
-
-    actions = [
-      "sts:AssumeRole",
-    ]
-
-    principals {
-      type        = "Service"
-      identifiers = [local.ec2_principal]
-    }
-  }
+locals{
+  managed_ng_assume_role_policy_json = jsonencode({
+      Statement = [
+        {
+            Action    = "sts:AssumeRole"
+            Effect    = "Allow"
+            Principal = {
+                Service = "${local.ec2_principal}"
+              }
+            Sid       = "EKSWorkerAssumeRole"
+          },
+      ]
+      Version   = "2012-10-17"
+    })
 }
