df53dea4596e5018e741d279ca47bb1f688c4950
Carlos Santana <csantana23@gmail.com>
2023-10-30 18:07:51 -0400

    getting started in tfstate path
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- patterns/gitops/getting-started-argocd/versions.tf
+++ patterns/gitops/getting-started-argocd/versions.tf
@@ -20,6 +20,6 @@ terraform {
   # backend "s3" {
   #   bucket = "terraform-ssp-github-actions-state"
   #   region = "us-west-2"
-  #   key    = "e2e/ipv4-prefix-delegation/terraform.tfstate"
+  #   key    = "e2e/getting-started-argocd/terraform.tfstate"
   # }
 }
