dec5a3743e64f92b49a7c12640f25124881032aa
Rishabh Jain <iamrishjain@gmail.com>
2023-10-30 17:31:19 -0700

    fix: add elb tag

--- modules/kubernetes-addons/aws-load-balancer-controller/data.tf
+++ modules/kubernetes-addons/aws-load-balancer-controller/data.tf
@@ -41,6 +41,7 @@ data "aws_iam_policy_document" "aws_lb" {
       "elasticloadbalancing:DescribeTargetGroupAttributes",
       "elasticloadbalancing:DescribeTargetGroups",
       "elasticloadbalancing:DescribeTargetHealth",
+      "elasticloadbalancing:AddTags"
     ]
   }
 
