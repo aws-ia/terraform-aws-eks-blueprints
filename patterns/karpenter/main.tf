5b36b1f1baa6f393c3f1e7cf14c33b116afca30a
Bryant Biggs <bryantbiggs@gmail.com>
2023-11-04 14:41:25 -0400

    fix: Update addons version for Karpenter fixes

--- patterns/karpenter/main.tf
+++ patterns/karpenter/main.tf
@@ -113,7 +113,7 @@ module "eks" {
 
 module "eks_blueprints_addons" {
   source  = "aws-ia/eks-blueprints-addons/aws"
-  version = "~> 1.10"
+  version = "~> 1.11"
 
   cluster_name      = module.eks.cluster_name
   cluster_endpoint  = module.eks.cluster_endpoint
