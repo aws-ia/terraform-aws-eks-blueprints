0f87167bd3db9affbc124508f3fa3c447651eab2
vchintal <vijay.chintalapati@gmail.com>
2024-03-01 12:25:17 -0800

    Removing the existing Istio multi-cluster pattern to prepare for replacement with a refined multi-vpc pattern

--- patterns/istio-multi-cluster/0.certs-tool/create-certs.sh
+++
@@ -1,6 +0,0 @@
-# https://istio.io/latest/docs/tasks/security/cert-management/plugin-ca-cert/#plug-in-certificates-and-key-into-the-cluster
-mkdir certs
-cd certs
-make -f ../Makefile.selfsigned.mk root-ca
-make -f ../Makefile.selfsigned.mk cluster-1-cacerts
-make -f ../Makefile.selfsigned.mk cluster-2-cacerts
