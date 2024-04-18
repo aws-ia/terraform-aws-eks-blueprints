5fa24b3db00db0e93e2d0cf59e5ef507323efe74
Bryant Biggs <bryantbiggs@gmail.com>
2024-04-18 15:21:49 -0400

    fix: Copy Kubecost assets to generated site directory, update formating for mkdocs rendering

--- .github/scripts/mkdocs-hooks.py
+++ .github/scripts/mkdocs-hooks.py
@@ -3,8 +3,10 @@ from  mkdocs.structure.files import File
 from pprint import pprint
 
 def on_page_markdown(markdown, **kwargs):
-    return markdown.replace('../../patterns/targeted-odcr/', './')
+    markdown.replace('../../patterns/targeted-odcr/', './')
+    markdown.replace('../../patterns/kubecost/', './')
 
+    return markdown
 
 def on_files(files, config, **kwargs):
     # Add targeted-odcr screenshots to the generated build
@@ -17,5 +19,14 @@ def on_files(files, config, **kwargs):
                 use_directory_urls=True
             )
         )
+
+    files.append(
+        File(
+            src_dir='./patterns/kubecost/assets/',
+            dest_dir=os.path.join(config.site_dir, 'patterns/kubecost/assets/'),
+            path='screenshot.png',
+            use_directory_urls=True
+        )
+    )
 
     return files
