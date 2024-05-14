341f9e072e784edd5784010239e0f5d307940281
Oleksandr Zaichenko <o.zaichenko@pin-up.tech>
2024-05-14 21:00:18 +0200

    Switch from data to var in locals for cluster name

--- modules/kubernetes-addons/locals.tf
+++ modules/kubernetes-addons/locals.tf
@@ -61,7 +61,7 @@ locals {
     aws_eks_cluster_endpoint       = local.eks_cluster_endpoint
     aws_partition_id               = data.aws_partition.current.partition
     aws_region_name                = data.aws_region.current.name
-    eks_cluster_id                 = data.aws_eks_cluster.eks_cluster.id
+    eks_cluster_id                 = var.eks_cluster_id
     eks_oidc_issuer_url            = local.eks_oidc_issuer_url
     eks_oidc_provider_arn          = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${local.eks_oidc_issuer_url}"
     tags                           = var.tags
