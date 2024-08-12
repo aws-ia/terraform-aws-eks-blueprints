09362cafe1fa378d0cf41d3db038668a33024e47
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2024-08-11 22:54:40 -0300

    Fix ingress,privatelink,sso,stateful,odcr,tls,wireguard

--- patterns/stateful/main.tf
+++ patterns/stateful/main.tf
@@ -29,7 +29,14 @@ provider "helm" {
 }
 
 data "aws_caller_identity" "current" {}
-data "aws_availability_zones" "available" {}
+
+data "aws_availability_zones" "available" {
+  # Do not include local zones
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
 
 locals {
   name   = basename(path.cwd)
