b7590118d39a2de0592f8d10160db9089138bea0
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2024-08-11 22:49:46 -0300

    Fix external-secrets,fargate,fully-private,gitos

--- patterns/external-secrets/main.tf
+++ patterns/external-secrets/main.tf
@@ -30,7 +30,14 @@ provider "kubectl" {
   }
 }
 
-data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {
+  # Do not include local zones
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
 data "aws_caller_identity" "current" {}
 
 locals {
--- patterns/gitops/multi-cluster-hub-spoke-argocd/hub/main.tf
+++ patterns/gitops/multi-cluster-hub-spoke-argocd/hub/main.tf
@@ -1,8 +1,16 @@
 provider "aws" {
   region = local.region
 }
+
 data "aws_caller_identity" "current" {}
-data "aws_availability_zones" "available" {}
+
+data "aws_availability_zones" "available" {
+  # Do not include local zones
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
 
 provider "helm" {
   kubernetes {
