a1a77ece60ee4b0fc6d4629f864236e7aec21c1f
kahirokunn <okinakahiro@gmail.com>
2024-09-18 10:38:00 +0900

    docs: correct spelling in comments

--- patterns/karpenter/main.tf
+++ patterns/karpenter/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = local.region
 }
 
-# This provider is required for ECR to autheticate with public repos. Please note ECR authetication requires us-east-1 as region hence its hardcoded below.
+# This provider is required for ECR to authenticate with public repos. Please note ECR authetication requires us-east-1 as region hence its hardcoded below.
 # If your region is same as us-east-1 then you can just use one aws provider
 provider "aws" {
   alias  = "ecr"
