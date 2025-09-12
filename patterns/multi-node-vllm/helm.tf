fc79c09c377507082de98cf88d42adae0ecf4279
Bryant Biggs <bryantbiggs@gmail.com>
2025-09-12 13:03:05 -0500

    feat: Update provider and module versons for multi-node vLLM pattern

--- patterns/multi-node-vllm/helm.tf
+++ patterns/multi-node-vllm/helm.tf
@@ -1,3 +1,17 @@
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
+
 ################################################################################
 # Device Plugin(s)
 ################################################################################
@@ -6,7 +20,7 @@ resource "helm_release" "nvidia_device_plugin" {
   name             = "nvidia-device-plugin"
   repository       = "https://nvidia.github.io/k8s-device-plugin"
   chart            = "nvidia-device-plugin"
-  version          = "0.17.1"
+  version          = "0.17.4"
   namespace        = "nvidia-device-plugin"
   create_namespace = true
   wait             = false
@@ -16,7 +30,7 @@ resource "helm_release" "aws_efa_device_plugin" {
   name       = "aws-efa-k8s-device-plugin"
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-efa-k8s-device-plugin"
-  version    = "v0.5.7"
+  version    = "v0.5.17"
   namespace  = "kube-system"
   wait       = false
 
@@ -36,8 +50,22 @@ resource "helm_release" "aws_efa_device_plugin" {
 # LWS (LeaderWorkerSet)
 ################################################################################
 
+provider "kubectl" {
+  apply_retry_count      = 5
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
 locals {
-  lws_version = "v0.5.1"
+  lws_version = "v0.6.3"
 }
 
 data "http" "lws" {
