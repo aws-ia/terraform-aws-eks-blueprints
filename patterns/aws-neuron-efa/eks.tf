570fdacd912343f711f2a4ee7b121cb01b4272b8
Bryant Biggs <bryantbiggs@gmail.com>
2025-09-12 12:47:53 -0500

    feat: Update provider and module versons for Neuron EFA pattern

--- patterns/aws-neuron-efa/eks.tf
+++ patterns/aws-neuron-efa/eks.tf
@@ -4,22 +4,17 @@
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.34"
+  version = "~> 21.0"
 
-  cluster_name    = local.name
-  cluster_version = "1.32"
+  name               = local.name
+  kubernetes_version = "1.33"
 
   # Give the Terraform identity admin access to the cluster
   # which will allow it to deploy resources into the cluster
   enable_cluster_creator_admin_permissions = true
-  cluster_endpoint_public_access           = true
+  endpoint_public_access                   = true
 
-  # These will become the default in the next major version of the module
-  bootstrap_self_managed_addons   = false
-  enable_irsa                     = false
-  enable_security_groups_for_pods = false
-
-  cluster_addons = {
+  addons = {
     coredns                   = {}
     eks-node-monitoring-agent = {}
     eks-pod-identity-agent = {
@@ -32,18 +27,8 @@ module "eks" {
     }
   }
 
-  # Add security group rules on the node group security group to
-  # allow EFA traffic
-  enable_efa_support = true
-
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-
-  eks_managed_node_group_defaults = {
-    node_repair_config = {
-      enabled = true
-    }
-  }
 
   eks_managed_node_groups = {
     neuron-efa = {
@@ -68,6 +53,10 @@ module "eks" {
           EOT
         }
       ]
+
+      node_repair_config = {
+        enabled = true
+      }
 
       min_size     = 2
       max_size     = 2
